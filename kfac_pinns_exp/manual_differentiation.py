d081aeb17df2f7f87b5c87f2b1133c38c96df3bb
Felix Dangel <48687646+f-dangel@users.noreply.github.com>
2023-11-17 14:44:18 -0500

    [ADD] Functionality to manually backpropagate gradients through layers (#2)
    * [ADD] Function to manually perform a forward pass
    
    * [ADD] Functionality to manually backpropagate through layers

--- kfac_pinns_exp/manual_differentiation.py
+++ kfac_pinns_exp/manual_differentiation.py
@@ -1,9 +1,10 @@
 """Functionality to manually compute derivatives of neural networks."""
 
-from typing import List
+from typing import List, Optional
 
-from torch import Tensor
-from torch.nn import Module
+from einops import einsum
+from torch import Tensor, ones_like, zeros_like
+from torch.nn import Linear, Module, Sigmoid
 
 
 def manual_forward(layers: List[Module], x: Tensor) -> List[Tensor]:
@@ -32,3 +33,91 @@ def manual_forward(layers: List[Module], x: Tensor) -> List[Tensor]:
         activations.append(x)
 
     return activations
+
+
+def manual_backward(
+    layers: List[Module],
+    activations: List[Tensor],
+    grad_output: Optional[Tensor] = None,
+) -> List[Tensor]:
+    """Backpropagate through all layers, computing gradients w.r.t. activations.
+
+    Args:
+        layers: A list of layers.
+        activations: A list of intermediate representations from the forward pass.
+            First entry is the original input, last entry is the last layer's output.
+        grad_output: The vector that is backpropagated through the layers. Must have
+            same size as the last element of `activations`. If not specified, a vector
+            of ones will be used.
+
+    Returns:
+        A list of gradients. First entry is the gradient of the output w.r.t. the input,
+        last entry is the gradient of the output w.r.t. the last layer's output, etc.
+
+    Raises:
+        ValueError: If the number of layers and activations do not match.
+        ValueError: If `grad_output` is specified but has incorrect size.
+    """
+    if len(layers) != len(activations) - 1:
+        raise ValueError(
+            f"Number of layers ({len(layers)}) must equal number of activations - 1. "
+            + f"got {len(activations)} activations."
+        )
+
+    grad_output = ones_like(activations[-1]) if grad_output is None else grad_output
+
+    gradients = [zeros_like(a) for a in activations[:-1]] + [grad_output]
+
+    # backpropagate, starting from the last layer
+    for i in range(len(layers), 0, -1):
+        layer = layers[i - 1]
+        inputs, outputs = activations[i - 1], activations[i]
+        grad_outputs = gradients[i]
+        gradients[i - 1] += manual_backward_layer(layer, inputs, outputs, grad_outputs)
+
+    return gradients
+
+
+def manual_backward_layer(
+    layer: Module, inputs: Tensor, outputs, grad_outputs: Tensor
+) -> Tensor:
+    """Backpropagate through a layer (output to input).
+
+    Args:
+        layer: The layer to backpropagate through.
+        inputs: The input to the layer from the forward pass.
+        outputs: The output of the layer from the forward pass.
+        grad_outputs: The gradient of the loss w.r.t. the layer's output.
+
+    Returns:
+        The gradient of the loss w.r.t. the layer's input. Has same
+        shape as the layer's input.
+
+    Raises:
+        NotImplementedError: If manual backpropagation for a layer is not implemented.
+        RuntimeError: If `grad_outputs` or the return value have incorrect shapes.
+    """
+    if grad_outputs.shape != outputs.shape:
+        raise RuntimeError(
+            "Grad output must have same shape as output. "
+            + f"Got {grad_outputs.shape} and {outputs.shape}."
+        )
+
+    if isinstance(layer, Linear):
+        # ... denotes an arbitrary number of additional dimensions, e.g. sequence length
+        grad_inputs = einsum(
+            layer.weight, grad_outputs, "d_out d_in, batch ... d_out -> batch ... d_in"
+        )
+    elif isinstance(layer, Sigmoid):
+        # σ' = σ(1 - σ)
+        grad_inputs = grad_outputs * outputs * (1 - outputs)
+    else:
+        raise NotImplementedError(f"Backpropagation through {layer} not implemented.")
+
+    if grad_inputs.shape != inputs.shape:
+        raise RuntimeError(
+            "Grad inputs must have same shape as inputs. "
+            + f"Got {grad_inputs.shape} and {inputs.shape}."
+        )
+
+    return grad_inputs
--- test/test_manual_differentiation.py
+++ test/test_manual_differentiation.py
@@ -1,12 +1,13 @@
 """Tests for `kfac_pinns_exp.manual_differentiation`."""
 
-from typing import Dict
+from typing import Dict, List, Tuple
 
 from pytest import mark
-from torch import manual_seed, rand
-from torch.nn import Linear, Sequential, Sigmoid
+from torch import Tensor, allclose, manual_seed, rand, rand_like
+from torch.autograd import grad
+from torch.nn import Linear, Module, Sequential, Sigmoid
 
-from kfac_pinns_exp.manual_differentiation import manual_forward
+from kfac_pinns_exp.manual_differentiation import manual_backward, manual_forward
 
 CASES = [
     {
@@ -19,6 +20,21 @@ CASES = [
 CASE_IDS = [case["id"] for case in CASES]
 
 
+def set_up(case: Dict) -> Tuple[List[Module], Tensor]:
+    """Set random seed and instantiate a case.
+
+    Args:
+        case: A dictionary describing a test case.
+
+    Returns:
+        A tuple of the instantiated layers and input.
+    """
+    manual_seed(case["seed"])
+    layers = case["layers_fn"]()
+    X = case["input_fn"]()
+    return layers, X
+
+
 @mark.parametrize("case", CASES, ids=CASE_IDS)
 def test_manual_forward(case: Dict):
     """Test manual execution of a forward pass.
@@ -28,14 +44,33 @@ def test_manual_forward(case: Dict):
     Args:
         case: A dictionary describing a test case.
     """
-    manual_seed(case["seed"])
-    layers = case["layers_fn"]()
-    X = case["input_fn"]()
+    layers, X = set_up(case)
 
     true_outputs = Sequential(*layers)(X)
 
     activations = manual_forward(layers, X)
     assert len(activations) == len(layers) + 1
+    assert allclose(activations[-1], true_outputs)
 
-    outputs = activations[-1]
-    assert outputs.shape == true_outputs.shape
+
+@mark.parametrize("case", CASES, ids=CASE_IDS)
+def test_manual_backward(case: Dict):
+    """Test manual execution of a backward pass.
+
+    Only checks for same gradient w.r.t. the first input. Does not check the
+    intermediate gradients.
+
+    Args:
+        case: A dictionary describing a test case.
+    """
+    layers, X = set_up(case)
+    X.requires_grad_(True)  # to compute gradients w.r.t. X
+
+    true_outputs = Sequential(*layers)(X)
+    grad_outputs = rand_like(true_outputs)
+    true_grad_X = grad(true_outputs, X, grad_outputs=grad_outputs)[0]
+
+    activations = manual_forward(layers, X)
+    gradients = manual_backward(layers, activations, grad_output=grad_outputs)
+    assert len(gradients) == len(layers) + 1
+    assert allclose(gradients[0], true_grad_X)
