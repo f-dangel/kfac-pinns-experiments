3712b92ecc850fe2cd3574a3d9444eae395e660b
Felix Dangel <48687646+f-dangel@users.noreply.github.com>
2023-11-20 11:51:52 -0500

    [ADD] Manual implementation of Gramian (#6)
    * [ADD] Function to manually perform a forward pass
    
    * [ADD] Functionality to manually backpropagate through layers
    
    * [ADD] Function to manually perform Hessian backpropagation
    
    * [ADD] Demo computing the Laplacian
    
    * [ADD] Compare functorch Gramian with hybrid implementation
    
    * [ADD] Manual implementation of the gradient

--- kfac_pinns_exp/exp01_manual_laplacian_and_fisher/demo_gramian.py
+++ kfac_pinns_exp/exp01_manual_laplacian_and_fisher/demo_gramian.py
@@ -3,7 +3,8 @@
 from typing import List
 
 from einops import einsum
-from torch import Tensor, allclose, autograd, manual_seed, rand, zeros, zeros_like
+from torch import Tensor, allclose, manual_seed, rand, zeros, zeros_like
+from torch.autograd import grad
 from torch.nn import Linear, Module, Sequential, Sigmoid
 
 from kfac_pinns_exp.autodiff_utils import autograd_gramian
@@ -46,11 +47,7 @@ def manual_laplace_autograd_gramian(
         laplacian = einsum(hessians[0], "batch d d ->")
 
         # compute the Laplacian's gradient with autodiff
-        grad_laplacian = autograd.grad(
-            laplacian,
-            param,
-            allow_unused=True,  # for bias terms
-        )[0]
+        grad_laplacian = grad(laplacian, param, allow_unused=True)[0]
 
         # The last layer's bias is not used in HBP, hence its  contribution to the
         # Gramian is zero. Autograd will return None for this bias.
@@ -64,6 +61,105 @@ def manual_laplace_autograd_gramian(
     return gramian_flat.reshape(*param.shape, *param.shape)
 
 
+def manual_gramian(
+    layers: List[Module], X: Tensor, layer_idx: int, param_name: str
+) -> Tensor:
+    """Compute the Gramian of the Laplacian of a parameter.
+
+    The Gramian is computed manually.
+
+    Args:
+        layers: List of layers in the model.
+        X: Input data. First dimension is batch dimension.
+        layer_idx: Index of the layer that contains the parameter we want to use to
+            compute the Gramian.
+        param_name: Name of the parameter we want to use to compute the Gramian.
+
+    Returns:
+        Gramian of the neural network's Laplacian w.r.t. the parameter. Has shape
+        `[*p.shape, *p.shape]` where `p` denotes the parameter.
+
+    Raises:
+        NotImplementedError: If the parameter does not live in a linear layer.
+    """
+    if not isinstance(layers[layer_idx], Linear):
+        raise NotImplementedError(
+            f"Gramian only supports Linear layers. Got {layers[layer_idx]}."
+        )
+
+    activations = manual_forward(layers, X)
+    gradients = manual_backward(layers, activations)
+    hessians = manual_hessian_backward(layers, activations, gradients)
+    laplacian = einsum(hessians[0], "batch d d ->")
+
+    # extract quantities required for the Gramian of a layer's parameter
+    layer_input = activations[layer_idx]
+    layer_output = activations[layer_idx + 1]
+    layer_grad_input = gradients[layer_idx]
+    layer_grad_output = gradients[layer_idx + 1]
+    layer_hess_input = hessians[layer_idx]
+    layer_hess_output = hessians[layer_idx + 1]
+
+    # compute the gradients w.r.t. all children of the parameter
+    # NOTE The first layer's grad_input and the last layer's output is not used in the
+    # Laplacian, hence `autograd` will return `None` for them
+    d_laplacian_d_hess_input, d_laplacian_d_grad_input, d_laplacian_d_output = grad(
+        laplacian, (layer_hess_input, layer_grad_input, layer_output), allow_unused=True
+    )
+
+    # use zero tensors for gradients of unused tensors
+    if d_laplacian_d_output is None:
+        d_laplacian_d_output = zeros_like(layer_output)
+    if d_laplacian_d_grad_input is None:
+        d_laplacian_d_grad_input = zeros_like(layer_grad_input)
+
+    # compute the Gramian
+    assert isinstance(layers[layer_idx], Linear)
+    param = getattr(layers[layer_idx], param_name)
+
+    if param_name == "bias":
+        # only the layer's output contributes
+        return einsum(
+            d_laplacian_d_output.detach(),
+            d_laplacian_d_output.detach(),
+            "batch d_out1, batch d_out2 -> d_out1 d_out2",
+        )
+
+    assert param_name == "weight"
+
+    # { ∂(Δu(xᵢ)) / ∂W | i = 1, ..., batch_size }
+    grads = zeros(X.shape[0], *param.shape, device=param.device, dtype=param.dtype)
+
+    # 1) contribution from layer's output (forward pass)
+    grads += einsum(
+        d_laplacian_d_output, layer_input, "batch d_out, batch d_in -> batch d_out d_in"
+    ).detach()
+
+    # 2) contribution from layer's input gradient (backward pass)
+    grads += einsum(
+        layer_grad_output,
+        d_laplacian_d_grad_input,
+        "batch d_out, batch d_in -> batch d_out d_in",
+    ).detach()
+
+    # 3) contribution from layers' input Hessian (Hessian backward pass)
+    grads += (
+        2
+        * einsum(
+            d_laplacian_d_hess_input,
+            param,
+            layer_hess_output,
+            "batch d_in1 d_in2, d_out1 d_in1, batch d_out1 d_out2-> batch d_out2 d_in2",
+        ).detach()
+    )
+
+    return einsum(
+        grads,
+        grads,
+        "batch d_out1 d_in1, batch d_out2 d_in2 -> d_out1 d_in1 d_out2 d_in2",
+    )
+
+
 def main():
     """Compute the Gramian for one weight matrix in a toy MLP.
 
@@ -71,7 +167,7 @@ def main():
 
     1) Computing the Laplacian manually and the Gramian automatically.
 
-    2) TODO Computing both the Laplacian and Gramian manually.
+    2) Computing both the Laplacian and Gramian manually.
 
     3) Computing both the Laplacian and Gramian automatically.
     """
@@ -98,7 +194,12 @@ def main():
             # 1) manual Laplacian, gradients for Gramian via autograd
             gramian1 = manual_laplace_autograd_gramian(layers, X, layer_idx, name)
 
-            # 2) TODO manual Laplacian and Gramian
+            # 2) manual Laplacian and Gramian
+            gramian2 = manual_gramian(layers, X, layer_idx, name)
+
+            same_1_2 = allclose(gramian1, gramian2)
+            print(f"\t\tsame(manual+auto, manual)? {same_1_2}")
+            assert same_1_2
 
             # 3) Laplacian and Gramian via autodiff (functorch)
             param_name = f"{layer_idx}.{name}"
