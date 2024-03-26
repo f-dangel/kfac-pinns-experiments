562710b695ce87d48c943b0755592b5dc93523c5
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-03-26 10:32:18 -0400

    [ADD] SGD sweep for Poisson5d [no ci]

---
+++ kfac_pinns_exp/exp10_reproduce_poisson5d/plot.py
@@ -0,0 +1,65 @@
+"""Plot the best runs from each tuned optimizer"""
+
+from matplotlib import pyplot as plt
+from palettable.colorbrewer import sequential
+from tueplots import bundles
+
+from kfac_pinns_exp.exp09_reproduce_poisson2d.download_best import load_best_run
+
+entity = "kfac-pinns"  # team name on wandb
+project = "poisson5d"  # name from the 'Projects' tab on wandb
+
+sweep_ids = {  # ids from the wandb agent
+    "tu585kr5": "SGD",
+    # "": "Adam",
+    # "": "ENGD (full)",
+    # "": "ENGD (layer-wise)",
+    # "": "ENGD (diagonal)",
+    # "": "Hessian-free",
+}
+
+# color options: https://jiffyclub.github.io/palettable/colorbrewer/
+colors = {
+    "SGD": sequential.Reds_4.mpl_colors[-2],
+    "Adam": sequential.Reds_4.mpl_colors[-1],
+    "ENGD (full)": sequential.Blues_5.mpl_colors[-3],
+    "ENGD (layer-wise)": sequential.Blues_5.mpl_colors[-2],
+    "ENGD (diagonal)": sequential.Blues_5.mpl_colors[-1],
+    "Hessian-free": sequential.Greens_4.mpl_colors[-2],
+    "KFAC": "black",
+}
+
+linestyles = {
+    "SGD": "-",
+    "Adam": "-",
+    "ENGD (full)": "-",
+    "ENGD (layer-wise)": "dashed",
+    "ENGD (diagonal)": "dotted",
+    "Hessian-free": "-",
+    "KFAC": "-",
+}
+
+if __name__ == "__main__":
+    with plt.rc_context(bundles.neurips2023(rel_width=1.0)):
+        fig, ax = plt.subplots(1, 1)
+        ax.set_xlabel("Iteration")
+        ax.set_xscale("log")
+        ax.set_ylabel("Loss")
+        ax.set_yscale("log")
+        ax.set_title("Poisson 5d")
+
+        for sweep_id, label in sweep_ids.items():
+            df_history, _ = load_best_run(
+                entity, project, sweep_id, save=True, update=True
+            )
+            ax.plot(
+                df_history["step"] + 1,
+                df_history["loss"],
+                label=label,
+                color=colors[label],
+                linestyle=linestyles[label],
+            )
+
+        ax.legend()
+
+        plt.savefig("poisson5d.pdf", bbox_inches="tight")
