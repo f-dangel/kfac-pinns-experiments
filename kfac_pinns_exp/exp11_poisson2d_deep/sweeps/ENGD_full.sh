7e6d1b842ebd4341e74cc74d5967eec421add8d8
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-03-27 15:28:30 -0400

    Create fresh sweeps [no ci]

--- kfac_pinns_exp/exp11_poisson2d_deep/sweeps/ENGD_full.sh
+++ kfac_pinns_exp/exp11_poisson2d_deep/sweeps/ENGD_full.sh
@@ -10,4 +10,4 @@
 source  ~/anaconda3/etc/profile.d/conda.sh
 conda activate kfac_pinns_exp
 
-wandb agent --count 1 kfac-pinns/poisson2d_deep/ogd3fp7p
+wandb agent --count 1 kfac-pinns/poisson2d_deep/d3o5edzz
