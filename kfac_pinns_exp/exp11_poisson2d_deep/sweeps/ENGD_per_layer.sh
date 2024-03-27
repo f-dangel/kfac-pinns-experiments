9d284907f58bdb153e40a979c09149bc923bde0f
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-03-27 15:24:25 -0400

    [FIX] Specify model in sweep configs [no ci]

--- kfac_pinns_exp/exp11_poisson2d_deep/sweeps/ENGD_per_layer.sh
+++ kfac_pinns_exp/exp11_poisson2d_deep/sweeps/ENGD_per_layer.sh
@@ -10,4 +10,4 @@
 source  ~/anaconda3/etc/profile.d/conda.sh
 conda activate kfac_pinns_exp
 
-wandb agent --count 1 kfac-pinns/poisson2d_deep/kat3vl07
+wandb agent --count 1 kfac-pinns/poisson2d_deep/29vcukxp
--- kfac_pinns_exp/exp11_poisson2d_deep/sweeps/LBFGS.sh
+++ kfac_pinns_exp/exp11_poisson2d_deep/sweeps/LBFGS.sh
@@ -10,4 +10,4 @@
 source  ~/anaconda3/etc/profile.d/conda.sh
 conda activate kfac_pinns_exp
 
-wandb agent --count 1 kfac-pinns/poisson2d_deep/1fqerdbm
+wandb agent --count 1 kfac-pinns/poisson2d_deep/n41mku5p
