fe90a1e319b5d1085ed16a6064c6d646eb3425bc
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-03-28 10:54:18 -0400

    [FIX] Sweep for ENGD-per-layer [no ci]

--- kfac_pinns_exp/exp10_reproduce_poisson5d/sweeps/ENGD_per_layer.sh
+++ kfac_pinns_exp/exp10_reproduce_poisson5d/sweeps/ENGD_per_layer.sh
@@ -10,4 +10,4 @@
 source  ~/anaconda3/etc/profile.d/conda.sh
 conda activate kfac_pinns_exp
 
-wandb agent --count 1 kfac-pinns/poisson5d/ig3b2jd5
+wandb agent --count 1 kfac-pinns/poisson5d/is65mq85
