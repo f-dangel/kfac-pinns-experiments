557be2ce600342fe0ed5f26e7e0c29c87c0ee6c2
Felix Dangel <48687646+f-dangel@users.noreply.github.com>
2024-04-01 16:33:29 -0400

    [REF] Move sweeps from `exp09` into folder (#38)

---
+++ kfac_pinns_exp/exp09_reproduce_poisson2d/create_sweeps.sh
@@ -0,0 +1,9 @@
+# Generate sweeps for all yaml config files
+# NOTE: This is usually only necessary once
+python yaml_to_sh.py sweeps/SGD.yaml sweeps/SGD.sh
+python yaml_to_sh.py sweeps/Adam.yaml sweeps/Adam.sh
+python yaml_to_sh.py sweeps/LBFGS.yaml sweeps/LBFGS.sh
+python yaml_to_sh.py sweeps/ENGD_diagonal.yaml sweeps/ENGD_diagonal.sh
+python yaml_to_sh.py sweeps/ENGD_full.yaml sweeps/ENGD_full.sh
+python yaml_to_sh.py sweeps/ENGD_per_layer.yaml sweeps/ENGD_per_layer.sh
+python yaml_to_sh.py sweeps/HessianFree.yaml sweeps/HessianFree.sh
