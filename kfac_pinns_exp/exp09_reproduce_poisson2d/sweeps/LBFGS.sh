a7ed9c87d70a786d26ea7a6a4f21d3127e7a2ac6
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-04-10 11:56:56 -0400

    [ADD] Fine-tune Adam, SGD, and LBFGS sweeps [no ci]

--- kfac_pinns_exp/exp09_reproduce_poisson2d/sweeps/LBFGS.sh
+++ kfac_pinns_exp/exp09_reproduce_poisson2d/sweeps/LBFGS.sh
@@ -10,4 +10,4 @@
 source  ~/anaconda3/etc/profile.d/conda.sh
 conda activate kfac_pinns_exp
 
-wandb agent --count 1 kfac-pinns/poisson2d/a11yxuw4
+wandb agent --count 1 kfac-pinns/poisson2d/765f4ehv
