7d41715f1149a668eb97bc4b8015fcf30cf94e3e
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-04-11 16:50:41 -0400

    [ADD] Update KFAC sweeps to check impact of refactoring [no ci]

--- kfac_pinns_exp/exp09_reproduce_poisson2d/sweeps/KFAC_empirical.sh
+++ kfac_pinns_exp/exp09_reproduce_poisson2d/sweeps/KFAC_empirical.sh
@@ -10,4 +10,4 @@
 source  ~/anaconda3/etc/profile.d/conda.sh
 conda activate kfac_pinns_exp
 
-wandb agent --count 1 kfac-pinns/poisson2d/frw6jqtf
+wandb agent --count 1 kfac-pinns/poisson2d/x4j52sam
