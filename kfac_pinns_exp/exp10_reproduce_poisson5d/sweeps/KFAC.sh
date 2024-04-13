09a8f0d590719fa2cc158b00bd5c2cef4fa03ba6
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-04-13 11:24:56 -0400

    [ADD] Refine KFAC sweeps based on first round [no ci]

--- kfac_pinns_exp/exp10_reproduce_poisson5d/sweeps/KFAC.sh
+++ kfac_pinns_exp/exp10_reproduce_poisson5d/sweeps/KFAC.sh
@@ -10,4 +10,4 @@
 source  ~/anaconda3/etc/profile.d/conda.sh
 conda activate kfac_pinns_exp
 
-wandb agent --count 1 kfac-pinns/poisson5d/ft6julz8
+wandb agent --count 1 kfac-pinns/poisson5d/j8cgxcma
--- kfac_pinns_exp/exp10_reproduce_poisson5d/sweeps/KFAC_forward_only.sh
+++ kfac_pinns_exp/exp10_reproduce_poisson5d/sweeps/KFAC_forward_only.sh
@@ -10,4 +10,4 @@
 source  ~/anaconda3/etc/profile.d/conda.sh
 conda activate kfac_pinns_exp
 
-wandb agent --count 1 kfac-pinns/poisson5d/1q3m46ag
+wandb agent --count 1 kfac-pinns/poisson5d/tbact0ls
