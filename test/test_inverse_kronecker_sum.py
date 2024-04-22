972402ba4b7df191a119f73f48eaa251d73c658d
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-04-22 15:08:52 -0400

    [REF] Shorten and test `matvec` of inverse Kronecker sum [no ci]

--- test/test_inverse_kronecker_sum.py
+++ test/test_inverse_kronecker_sum.py
@@ -1,6 +1,7 @@
 """Test class for inverting the sum of two Kronecker products."""
 
-from torch import allclose, eye, manual_seed, rand
+from pytest import mark
+from torch import allclose, eye, inverse, kron, manual_seed, rand
 
 from kfac_pinns_exp.inverse_kronecker_sum import InverseKroneckerSum
 
@@ -11,11 +12,11 @@ def test_InverseKroneckerSum_eigh():
     D = 100
 
     # create symmetric positive definite matrices
-    damping = 1e-8 * eye(D).double()
+    damping_mat = 1e-8 * eye(D).double()
     A = rand(D, D).double()
-    A = A @ A.T + damping
+    A = A @ A.T + damping_mat
     B = rand(D, D).double()
-    B = B @ B.T + damping
+    B = B @ B.T + damping_mat
 
     # compare their general eigendecompositions among backends
     evals_scipy, evecs_scipy, B_inv_scipy = InverseKroneckerSum.eigh(
@@ -31,3 +32,44 @@ def test_InverseKroneckerSum_eigh():
         evec_scipy, evec_torch = evecs_scipy[:, d], evecs_torch[:, d]
         assert allclose(evec_scipy, evec_torch) or allclose(-evec_scipy, evec_torch)
     assert allclose(B_inv_scipy, B_inv_torch)
+
+
+BACKENDS = ["scipy", "torch"]
+BACKEND_IDS = [f"backend={backend}" for backend in BACKENDS]
+
+
+@mark.parametrize("backend", BACKENDS, ids=BACKEND_IDS)
+def test_InverseKroneckerSum__matmul__(backend: str):
+    """Test matrix-vector multiplication with inverse of a Kronecker sum.
+
+    Args:
+        backend: Backend for the eigendecomposition.
+    """
+    manual_seed(0)
+    D1, D2 = 9, 11
+
+    # create symmetric positive definite matrices
+    damping_D1_mat = 1e-8 * eye(D1).double()
+    damping_D2_mat = 1e-8 * eye(D2).double()
+
+    A1 = rand(D1, D1).double()
+    A1 = A1 @ A1.T + damping_D1_mat
+
+    A2 = rand(D2, D2).double()
+    A2 = A2 @ A2.T + damping_D2_mat
+
+    B1 = rand(D1, D1).double()
+    B1 = B1 @ B1.T + damping_D1_mat
+
+    B2 = rand(D2, D2).double()
+    B2 = B2 @ B2.T + damping_D2_mat
+
+    # compute, damp, and invert the Kronecker sum manually
+    manual_inv = inverse(kron(A1, A2) + kron(B1, B2))
+    kronecker_inv = InverseKroneckerSum(A1, A2, B1, B2, backend=backend)
+
+    # create a random vector for multiplication and compare matvecs
+    x = rand(D1 * D2).double()
+    manual_matvec = manual_inv @ x
+    kronecker_matvec = kronecker_inv @ x
+    assert allclose(manual_matvec, kronecker_matvec)
