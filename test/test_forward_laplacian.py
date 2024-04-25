988e6b0226b2b2b470b6136f89108c261a874a78
Felix Dangel <48687646+f-dangel@users.noreply.github.com>
2024-04-25 14:03:44 -0400

    [ADD] Option to compute the Laplacian on a sub-set of coordinates (#48)
    * [ADD] Fine-tune sweeps on Poisson5d, add KFAC sweeps [no ci]
    
    * [ADD] Fine-tune sweeps on Poisson2d-deep, add KFAC sweeps [no ci]
    
    * [ADD] Refine KFAC sweeps based on first round [no ci]
    
    * [REF] Shorten and test `matvec` of inverse Kronecker sum [no ci]
    
    * [ADD] Sweeps for Poisson5d on a deep net [no ci]
    
    * [ADD] Update Poisson5d with fine-tuned sweeps [no ci]
    
    * [ADD] Update Poisson2d deep with fine-tuned sweeps [no ci]
    
    * [ADD] Log command with wandb
    
    * [ADD] Change Poisson5d to use sum of cos boundary conditions [no ci]
    
    * [ADD] Change Poisson5d-deep to sum of cos boundary conditions [no ci]
    
    * [REF] Update sweep ids in Poisson5d plotting script
    
    * [ADD] Plotting script for Poisson5d deep
    
    * [REF] Launch sweeps with all available qos values to maximize GPU usage [no ci]
    
    * [ADD] Print command [no ci]
    
    * [ADD] Include t4v1 and t4v2 in partitions to run jobs on [no ci]
    
    * [TEST] Run plotting script of exp12
    
    * [ADD] Option to specify the coordinates over which the Laplacian is computed [no ci]
    
    * [FIX] Linters
    
    * [DEL] Outdated runs

--- test/test_forward_laplacian.py
+++ test/test_forward_laplacian.py
@@ -1,32 +1,47 @@
 """Test `kfac_pinns_exp.forward_laplacian`."""
 
 from test.test_manual_differentiation import CASE_IDS, CASES, set_up
-from typing import Dict
+from typing import Callable, Dict, List, Union
 
 from einops import einsum
 from pytest import mark
-from torch import allclose
+from torch import Tensor, allclose
 from torch.nn import Sequential
 
 from kfac_pinns_exp.autodiff_utils import autograd_input_hessian
 from kfac_pinns_exp.forward_laplacian import manual_forward_laplacian
 
+COORDINATE_FNS = {
+    "coordinates=None": lambda X: None,
+    "coordinates=even": lambda X: [i for i in range(X.shape[1]) if i % 2 == 0],
+}
 
+
+@mark.parametrize("coordinate_fn", COORDINATE_FNS.values(), ids=COORDINATE_FNS.keys())
 @mark.parametrize("case", CASES, ids=CASE_IDS)
-def test_manual_forward_laplacian(case: Dict):
+def test_manual_forward_laplacian(
+    case: Dict, coordinate_fn: Callable[[Tensor], Union[None, List[int]]]
+):
     """Compute the forward Laplacian and compare with functorch.
 
     Args:
         case: A dictionary describing a test case.
+        coordinate_fn: A function that takes the input data `X` and returns
+            the coordinates whose diagonal entries are summed into the Laplacian.
     """
     layers, X = set_up(case)
+    coordinates = coordinate_fn(X)
 
     # automatic computation (via functorch)
-    true_hessian_X = autograd_input_hessian(Sequential(*layers), X)
+    true_hessian_X = autograd_input_hessian(
+        Sequential(*layers), X, coordinates=coordinates
+    )
+    if coordinates is not None:
+        assert true_hessian_X.shape[1:] == (len(coordinates), len(coordinates))
     true_laplacian_X = einsum(true_hessian_X, "batch d d -> ")
 
     # forward-Laplacian computation
-    coefficients = manual_forward_laplacian(layers, X)
+    coefficients = manual_forward_laplacian(layers, X, coordinates=coordinates)
     laplacian_X = einsum(coefficients[-1]["laplacian"], "n d -> ")
 
     assert allclose(true_laplacian_X, laplacian_X)
