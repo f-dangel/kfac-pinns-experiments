2cf83a3afb52c9ee9551c743254b8dba392a52a9
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-04-26 08:58:19 -0400

    [ADD] Plotting script [no ci]

--- kfac_pinns_exp/exp09_reproduce_poisson2d/yaml_to_sh.py
+++ kfac_pinns_exp/exp09_reproduce_poisson2d/yaml_to_sh.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
 #SBATCH --cpus-per-task=4
 #SBATCH --time=02:00:00
 #SBATCH --mem-per-cpu=8G
-#SBATCH --array=1-64
+#SBATCH --array=1-64%5
 
 source  ~/anaconda3/etc/profile.d/conda.sh
 conda activate kfac_pinns_exp
