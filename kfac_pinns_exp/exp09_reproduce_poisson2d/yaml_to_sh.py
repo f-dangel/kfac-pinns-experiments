854662184494f72feda8f5901a3ab023d2560d6e
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-04-28 22:37:32 -0400

    [ADD] QOS option to `yaml_to_sh.py` script [no ci]

--- kfac_pinns_exp/exp09_reproduce_poisson2d/yaml_to_sh.py
+++ kfac_pinns_exp/exp09_reproduce_poisson2d/yaml_to_sh.py
@@ -4,13 +4,28 @@ from argparse import ArgumentParser
 
 from kfac_pinns_exp.utils import run_verbose
 
+QUEUE_TO_TIME = {
+    "normal": "16:00:00",
+    "m": "12:00:00",
+    "m2": "08:00:00",
+    "m3": "04:00:00",
+    "m4": "02:00:00",
+    "m5": "01:00:00",
+}
+
 if __name__ == "__main__":
     parser = ArgumentParser(
         description="Create shell script from sweep .yaml configuration."
     )
     parser.add_argument("yaml_file", type=str, help="Path to the .yaml file.")
     parser.add_argument("sweep_name", type=str, help="Name of the sweep.")
-
+    parser.add_argument(
+        "--qos",
+        type=str,
+        choices=QUEUE_TO_TIME.keys(),
+        default="m4",
+        help="Slurm QOS for the job.",
+    )
     args = parser.parse_args()
 
     cmd = [
@@ -24,30 +39,26 @@ if __name__ == "__main__":
     # run the wandb command
     job = run_verbose(cmd)
 
-    # get the command from stderr
-    trigger = "wandb: Run sweep agent with: wandb agent "
     lines = job.stderr.split("\n")
+    trigger = "wandb: Run sweep agent with: wandb agent "
     (line,) = [line for line in lines if line.startswith(trigger)]
     line = line.replace(trigger, "")
     line = f"wandb agent --count 1 {line}"
 
     # create the .sh file
-    TEMPLATE = (
-        r"""#!/bin/bash
+    TEMPLATE = f"""#!/bin/bash
 #SBATCH --partition=rtx6000,t4v1,t4v2
-#SBATCH --qos=m4
+#SBATCH --qos={args.qos}
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
-#SBATCH --time=02:00:00
+#SBATCH --time={QUEUE_TO_TIME[args.qos]}
 #SBATCH --mem-per-cpu=8G
-#SBATCH --array=1-64%10
+#SBATCH --array=1-64%16
 
 source  ~/anaconda3/etc/profile.d/conda.sh
 conda activate kfac_pinns_exp
 
-"""
-        + line
-    )
+{line}"""
 
     sh_file = args.yaml_file.replace(".yaml", ".sh")
     with open(sh_file, "w") as f:
