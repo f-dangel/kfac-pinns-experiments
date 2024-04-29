64bdb4913ac6fcffc22b1b0477dbb772d3c3bab7
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-04-28 22:16:24 -0400

    [REF] Re-create KFAC Poisson2d sweeps [no ci]

--- kfac_pinns_exp/exp09_reproduce_poisson2d/sweeps/KFAC.sh
+++ kfac_pinns_exp/exp09_reproduce_poisson2d/sweeps/KFAC.sh
@@ -10,4 +10,4 @@
 source  ~/anaconda3/etc/profile.d/conda.sh
 conda activate kfac_pinns_exp
 
-wandb agent --count 1 kfac-pinns/poisson2d/i7kb12iu
+wandb agent --count 1 kfac-pinns/poisson2d/685m4syn
