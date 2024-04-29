2566d9b8e998c29e9de954eee8dedbf391f33fc9
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-04-29 18:36:34 -0400

    [FIX] Tune initialization for ENGD-per-layer [no ci]

--- kfac_pinns_exp/exp09_reproduce_poisson2d/sweeps/ENGD_per_layer.sh
+++ kfac_pinns_exp/exp09_reproduce_poisson2d/sweeps/ENGD_per_layer.sh
@@ -10,4 +10,4 @@
 source  ~/anaconda3/etc/profile.d/conda.sh
 conda activate kfac_pinns_exp
 
-wandb agent --count 1 kfac-pinns/poisson2d/n2i5uo6w
+wandb agent --count 1 kfac-pinns/poisson2d/h7uhm3nj
