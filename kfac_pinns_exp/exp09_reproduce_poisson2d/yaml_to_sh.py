b86fd7540b23cb6e6af55b21c1b05ba0adc5c65a
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-04-28 21:41:58 -0400

    [ADD] Use random search in KFAC sweeps [no ci]

--- kfac_pinns_exp/exp09_reproduce_poisson2d/yaml_to_sh.py
+++ kfac_pinns_exp/exp09_reproduce_poisson2d/yaml_to_sh.py
@@ -35,11 +35,12 @@ if __name__ == "__main__":
     TEMPLATE = (
         r"""#!/bin/bash
 #SBATCH --partition=rtx6000,t4v1,t4v2
+#SBATCH --qos=m4
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --time=02:00:00
 #SBATCH --mem-per-cpu=8G
-#SBATCH --array=1-64%5
+#SBATCH --array=1-64%10
 
 source  ~/anaconda3/etc/profile.d/conda.sh
 conda activate kfac_pinns_exp
