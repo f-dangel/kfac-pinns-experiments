912b125b627ac498812fc5db90633854de2fc393
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-04-30 19:32:18 -0400

    [ADD] Time-limited sweeps for heat1d [no ci]

--- kfac_pinns_exp/exp13_reproduce_heat1d/create_sweeps.sh
+++ kfac_pinns_exp/exp13_reproduce_heat1d/create_sweeps.sh
@@ -3,10 +3,10 @@
 python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/SGD.yaml sweeps/SGD.sh --qos=m5
 python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/Adam.yaml sweeps/Adam.sh --qos=m5
 python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/LBFGS.yaml sweeps/LBFGS.sh --qos=m5
-python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/ENGD_diagonal.yaml sweeps/ENGD_diagonal.sh --qos=m4
-python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/ENGD_full.yaml sweeps/ENGD_full.sh --qos=m4
-python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/ENGD_per_layer.yaml sweeps/ENGD_per_layer.sh --qos=m4
-python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/HessianFree.yaml sweeps/HessianFree.sh --qos=m4
+python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/ENGD_diagonal.yaml sweeps/ENGD_diagonal.sh --qos=m5
+python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/ENGD_full.yaml sweeps/ENGD_full.sh --qos=m5
+python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/ENGD_per_layer.yaml sweeps/ENGD_per_layer.sh --qos=m5
+python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/HessianFree.yaml sweeps/HessianFree.sh --qos=m5
 python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/KFAC.yaml sweeps/KFAC.sh --qos=m5
 python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/KFAC_empirical.yaml sweeps/KFAC_empirical.sh --qos=m5
 python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/KFAC_forward_only.yaml sweeps/KFAC_forward_only.sh --qos=m5
