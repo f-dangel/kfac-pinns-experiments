e17757a00b3a22ad2579c813def575ba51154cb0
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-05-03 17:34:49 -0400

    [FIX] Set momentum to zero [no ci]

--- kfac_pinns_exp/exp12_poisson5d_deep/sweeps/KFAC_auto.sh
+++ kfac_pinns_exp/exp12_poisson5d_deep/sweeps/KFAC_auto.sh
@@ -10,4 +10,4 @@
 source  ~/anaconda3/etc/profile.d/conda.sh
 conda activate kfac_pinns_exp
 
-wandb agent --count 1 kfac-pinns/poisson5d_deep/d2phdjo6
+wandb agent --count 1 kfac-pinns/poisson5d_deep/xvizro3s
