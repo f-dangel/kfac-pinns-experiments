788a77e483f68c9cdc0968bff47cb1de1d7adad5
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-05-04 10:50:28 -0400

    [ADD] KFAC-auto sweeps for Poisson5d [no ci]

--- kfac_pinns_exp/exp10_reproduce_poisson5d/create_sweeps.sh
+++ kfac_pinns_exp/exp10_reproduce_poisson5d/create_sweeps.sh
@@ -10,3 +10,6 @@ python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/HessianFree.yaml sweeps
 python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/KFAC.yaml sweeps/KFAC.sh --qos=m5
 python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/KFAC_empirical.yaml sweeps/KFAC_empirical.sh --qos=m5
 python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/KFAC_forward_only.yaml sweeps/KFAC_forward_only.sh --qos=m5
+python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/KFAC_auto.yaml sweeps/KFAC_auto.sh --qos=m5
+python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/KFAC_empirical_auto.yaml sweeps/KFAC_empirical_auto.sh --qos=m5
+python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/KFAC_forward_only_auto.yaml sweeps/KFAC_forward_only_auto.sh --qos=m5
