10616a18798ec00d74b374782118d704da1535fe
Marius Zeinhofer <marius.zeinhofer@gmail.com>
2024-05-04 09:58:30 +0200

    [ADD] the yaml files for weinan 100d Poisson

--- kfac_pinns_exp/exp14_poisson_100d_weinan/sweeps/Adam.sh
+++ kfac_pinns_exp/exp14_poisson_100d_weinan/sweeps/Adam.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#SBATCH --partition=rtx6000,t4v1,t4v2
+#SBATCH --partition=rtx6000
+#SBATCH --qos=m4
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --time=02:00:00
 #SBATCH --mem-per-cpu=8G
-#SBATCH --array=1-64%5
+#SBATCH --array=1-64%17
 
 source  ~/anaconda3/etc/profile.d/conda.sh
 conda activate kfac_pinns_exp
 
-wandb agent --count 1 kfac-pinns/weinan_norm/elysfp1f
+wandb agent --count 1 kfac-pinns/weinan_norm/jb2nwejd
