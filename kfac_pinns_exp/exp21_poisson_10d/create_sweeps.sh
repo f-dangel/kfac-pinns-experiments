f03b636153ff53b6f5205a58de415c2709a280c4
Marius Zeinhofer <marius.zeinhofer@gmail.com>
2024-05-11 23:15:01 +0200

    [ADD] experiment21 for 10d poisson on the u_weinan solution

---
+++ kfac_pinns_exp/exp21_poisson_10d/create_sweeps.sh
@@ -0,0 +1,12 @@
+# Generate sweeps for all yaml config files
+# NOTE: This is usually only necessary once
+python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/SGD.yaml sweeps/SGD.sh --qos=m5
+python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/Adam.yaml sweeps/Adam.sh --qos=m5
+python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/LBFGS.yaml sweeps/LBFGS.sh --qos=m5
+python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/HessianFree.yaml sweeps/HessianFree.sh --qos=m5
+python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/KFAC.yaml sweeps/KFAC.sh --qos=m5
+python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/KFAC_empirical.yaml sweeps/KFAC_empirical.sh --qos=m5
+python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/KFAC_forward_only.yaml sweeps/KFAC_forward_only.sh --qos=m5
+python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/KFAC_auto.yaml sweeps/KFAC_auto.sh --qos=m5
+python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/KFAC_empirical_auto.yaml sweeps/KFAC_empirical_auto.sh --qos=m5
+python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/KFAC_forward_only_auto.yaml sweeps/KFAC_forward_only_auto.sh --qos=m5
