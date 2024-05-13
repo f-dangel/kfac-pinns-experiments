2a4bb87a4ea65be385d4e620425676c5d43d8bb6
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-05-13 18:57:40 +0200

    [FIX] Indentation in Adam sweep [no ci]

--- kfac_pinns_exp/exp19_poisson5d_mlp_tanh_256/sweeps/Adam.sh
+++ kfac_pinns_exp/exp19_poisson5d_mlp_tanh_256/sweeps/Adam.sh
@@ -12,4 +12,4 @@ echo "[DEBUG] Host name: " `hostname`
 source  ~/anaconda3/etc/profile.d/conda.sh
 conda activate kfac_pinns_exp
 
-wandb agent --count 1 kfac-pinns/poisson5d_mlp_tanh_256/buz3wb3w
+wandb agent --count 1 kfac-pinns/poisson5d_mlp_tanh_256/0sjzv0xk
