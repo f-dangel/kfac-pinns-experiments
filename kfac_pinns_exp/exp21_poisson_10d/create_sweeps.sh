d0c164649225f6fea349fa264f5fc76f93f9ab0a
Marius Zeinhofer <marius.zeinhofer@gmail.com>
2024-05-13 12:36:34 +0200

    [FIX] remove the kfac variants that we don't use anymore

--- kfac_pinns_exp/exp21_poisson_10d/create_sweeps.sh
+++ kfac_pinns_exp/exp21_poisson_10d/create_sweeps.sh
@@ -5,8 +5,4 @@ python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/Adam.yaml sweeps/Adam.s
 python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/LBFGS.yaml sweeps/LBFGS.sh --qos=m4
 python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/HessianFree.yaml sweeps/HessianFree.sh --qos=m4
 python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/KFAC.yaml sweeps/KFAC.sh --qos=m4
-python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/KFAC_empirical.yaml sweeps/KFAC_empirical.sh --qos=m4
-python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/KFAC_forward_only.yaml sweeps/KFAC_forward_only.sh --qos=m4
 python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/KFAC_auto.yaml sweeps/KFAC_auto.sh --qos=m4
-python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/KFAC_empirical_auto.yaml sweeps/KFAC_empirical_auto.sh --qos=m4
-python ../exp09_reproduce_poisson2d/yaml_to_sh.py sweeps/KFAC_forward_only_auto.yaml sweeps/KFAC_forward_only_auto.sh --qos=m4
--- kfac_pinns_exp/exp21_poisson_10d/launch_sweeps.sh
+++ kfac_pinns_exp/exp21_poisson_10d/launch_sweeps.sh
@@ -3,12 +3,8 @@ cd sweeps/
 
 # launch each script
 sbatch KFAC.sh
-sbatch KFAC_empirical.sh
-sbatch KFAC_forward_only.sh
 sbatch SGD.sh
 sbatch Adam.sh
 sbatch LBFGS.sh
 sbatch HessianFree.sh
 sbatch KFAC_auto.sh
-sbatch KFAC_empirical_auto.sh
-sbatch KFAC_forward_only_auto.sh
