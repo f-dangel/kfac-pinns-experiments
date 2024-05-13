0ccb8bbd161bdc3b34427e2a1b1276d4adf62428
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-05-13 14:55:25 +0200

    [FIX] Test

--- test/exp24_heat1d_groupplot/test_exp24_plot.py
+++ test/exp24_heat1d_groupplot/test_exp24_plot.py
@@ -1,4 +1,4 @@
-"""Run the plotting script from exp20."""
+"""Run the plotting script from exp24."""
 
 from kfac_pinns_exp.exp24_heat1d_groupplot import plot
 from kfac_pinns_exp.utils import run_verbose
@@ -6,5 +6,5 @@ from kfac_pinns_exp.utils import run_verbose
 
 def test_execute_plot():
     """Execute the plotting script."""
-    cmd = ["python", plot.__file__, "--local_files", "--disable_tex"]
+    cmd = ["python", plot.__file__, "--disable_tex"]
     run_verbose(cmd)
