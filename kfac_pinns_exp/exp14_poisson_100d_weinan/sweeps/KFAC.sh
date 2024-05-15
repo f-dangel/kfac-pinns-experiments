88ad4f69d18dea119f979502e6545eb710623450
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-05-15 13:36:51 +0200

    [ADD] Re-create Poisson100d sweeps [no ci]

--- kfac_pinns_exp/exp14_poisson_100d_weinan/sweeps/KFAC.sh
+++ kfac_pinns_exp/exp14_poisson_100d_weinan/sweeps/KFAC.sh
@@ -13,4 +13,4 @@ echo "[DEBUG] Host name: " `hostname`
 source  ~/anaconda3/etc/profile.d/conda.sh
 conda activate kfac_pinns_exp
 
-wandb agent --count 1 kfac-pinns/poisson100d_weinan_norm/hugqtyml
+wandb agent --count 1 kfac-pinns/poisson100d_weinan_norm/37t4mqb0
