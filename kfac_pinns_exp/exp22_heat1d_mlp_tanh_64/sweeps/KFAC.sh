0bb0e9e615e6e8caf4e63c740cd45354e642c139
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-05-15 10:32:45 +0200

    [FIX] Restart fine-tuned sweeps from power failure of heat1d medium [no ci]

--- kfac_pinns_exp/exp22_heat1d_mlp_tanh_64/sweeps/KFAC.sh
+++ kfac_pinns_exp/exp22_heat1d_mlp_tanh_64/sweeps/KFAC.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --partition=rtx6000
-#SBATCH --qos=deadline
-#SBATCH --account=deadline
+#SBATCH --qos=m5
+
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
-#SBATCH --time=12:00:00
+#SBATCH --time=01:00:00
 #SBATCH --mem-per-cpu=8G
 #SBATCH --array=1-64%17
 
@@ -13,4 +13,4 @@ echo "[DEBUG] Host name: " `hostname`
 source  ~/anaconda3/etc/profile.d/conda.sh
 conda activate kfac_pinns_exp
 
-wandb agent --count 1 kfac-pinns/heat1d_mlp_tanh_64/3mz931ye
+wandb agent --count 1 kfac-pinns/heat1d_mlp_tanh_64/80e1edz8
