8c5f1383f81fa4272b285b68571647ffb5ed6ec1
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-06-04 16:13:31 -0400

    [ADD] Generalize forward Laplacian to weighted sum of second derivatives

--- kfac_pinns_exp/forward_laplacian.py
+++ kfac_pinns_exp/forward_laplacian.py
@@ -8,9 +8,12 @@ from torch.nn import Linear, Module, ReLU, Sigmoid, Tanh
 
 
 def manual_forward_laplacian(
-    layers: List[Module], x: Tensor, coordinates: Optional[List[int]] = None
+    layers: List[Module],
+    x: Tensor,
+    coordinates: Optional[List[int]] = None,
+    coefficients: Optional[Tensor] = None,
 ) -> List[Dict[str, Tensor]]:
-    """Compute the NN prediction and Laplacian in one forward pass.
+    """Compute the NN prediction and Laplacian (or weighted sum of second derivatives).
 
     Args:
         layers: A list of layers defining the NN.
@@ -18,6 +21,10 @@ def manual_forward_laplacian(
         coordinates: List of indices specifying the Hessian diagonal entries
             that are summed into the Laplacian. If `None`, all diagonal entries
             are summed. Default: `None`.
+        coefficients: A coefficient matrix that, if specified, will compute a weighted
+            sum `∑ᵢⱼ cᵢⱼ ∂²f/∂xᵢ∂xⱼ` instead of the Laplacian. If `None`, the Laplacian is
+            computed, i.e. `cᵢⱼ = δᵢⱼ`. If `coordinates` is specified, the coefficients
+            must match the size of the sub-space implied by `coordinates`.
 
     Returns:
         A list of dictionaries, each containing the Taylor coefficients (0th-, 1st-, and
@@ -26,6 +33,7 @@ def manual_forward_laplacian(
     Raises:
         ValueError: If a layer uses in-place operations.
         ValueError: If coordinates are not unique or out of range.
+        ValueError: If the coefficients shape does not match the expected shape.
     """
     # inialize Taylor coefficients
     batch_size, feature_dims = x.shape[0], x.shape[1:]
@@ -36,7 +44,7 @@ def manual_forward_laplacian(
         .repeat(batch_size, 1, 1)
     ).reshape(batch_size, num_features, *feature_dims)
     laplacian_init = zeros_like(x)
-    coefficients = {
+    derivatives = {
         "forward": x,
         "directional_gradients": directional_grad_init,
         "laplacian": laplacian_init,
@@ -53,33 +61,49 @@ def manual_forward_laplacian(
                 f" Got {coordinates}."
             )
 
+    if coefficients is not None:
+        dim = num_features if coordinates is None else len(coordinates)
+        expected_shape = (dim, dim)
+        if coefficients.shape != expected_shape:
+            raise ValueError(
+                f"Expected coefficients of shape {expected_shape}."
+                f" Got {coefficients.shape}."
+            )
+
     # pass Taylor coefficients through the network
-    result = [coefficients]
+    result = [derivatives]
 
     for layer in layers:
         if getattr(layer, "inplace", False):
             raise ValueError(
                 f"Layers with in-place operations are not supported. Got {layer}."
             )
-        coefficients = manual_forward_laplacian_layer(
-            layer, coefficients, coordinates=coordinates
+        derivatives = manual_forward_laplacian_layer(
+            layer, derivatives, coordinates=coordinates, coefficients=coefficients
         )
-        result.append(coefficients)
+        result.append(derivatives)
 
     return result
 
 
 def manual_forward_laplacian_layer(
-    layer: Module, coefficients: Dict[str, Tensor], coordinates: Union[List[int], None]
+    layer: Module,
+    derivatives: Dict[str, Tensor],
+    coordinates: Union[List[int], None],
+    coefficients: Union[Tensor, None],
 ) -> Dict[str, Tensor]:
     """Propagate the 0th, 1st, and summed 2nd-order Taylor coefficients through a layer.
 
     Args:
         layer: The layer to propagate the Taylor coefficients through.
-        coefficients: A dictionary containing the Taylor coefficients.
+        derivatives: A dictionary containing the Taylor coefficients.
         coordinates: List of indices specifying the Hessian diagonal entries
             that are summed into the Laplacian. If `None`, all diagonal entries
             are summed.
+        coefficients: A coefficient matrix that, if specified, will compute a weighted
+            sum `∑ᵢⱼ cᵢⱼ ∂²f/∂xᵢ∂xⱼ` instead of the Laplacian. If `None`, the Laplacian is
+            computed, i.e. `cᵢⱼ = δᵢⱼ`. If `coordinates` is specified, the coefficients
+            must match the size of the sub-space implied by `coordinates`.
 
     Returns:
         A dictionary containing the new Taylor coefficients.
@@ -87,13 +111,13 @@ def manual_forward_laplacian_layer(
     Raises:
         NotImplementedError: If the layer type is not supported.
     """
-    old_forward = coefficients["forward"]
-    old_directional_gradients = coefficients["directional_gradients"]
-    old_laplacian = coefficients["laplacian"]
+    old_forward = derivatives["forward"]
+    old_directional_gradients = derivatives["directional_gradients"]
+    old_laplacian = derivatives["laplacian"]
 
     new_forward = layer(old_forward)
 
-    if isinstance(layer, Linear):
+    if isinstance(layer, Linear):  # linear layers
         new_directional_gradients = einsum(
             layer.weight,
             old_directional_gradients,
@@ -102,36 +126,45 @@ def manual_forward_laplacian_layer(
         new_laplacian = einsum(
             layer.weight, old_laplacian, "d_out d_in, ... d_in -> ... d_out"
         )
-    elif isinstance(layer, Sigmoid):
-        jac = new_forward * (1 - new_forward)
-        hess = jac * (1 - 2 * new_forward)
+    elif isinstance(layer, (Sigmoid, ReLU, Tanh)):  # element-wise activations
+        if isinstance(layer, Sigmoid):
+            jac = new_forward * (1 - new_forward)
+            hess = jac * (1 - 2 * new_forward)
+        elif isinstance(layer, ReLU):
+            jac = gt(old_forward, 0).to(old_forward.dtype)
+            hess = None
+        elif isinstance(layer, Tanh):
+            jac = 1 - new_forward**2
+            hess = -2 * new_forward * jac
+
         new_directional_gradients = einsum(
             old_directional_gradients, jac, "n d0 ..., n ... -> n d0 ..."
         )
-        # only use the relevant coordinates for the Laplacian
-        if coordinates is not None:
-            old_directional_gradients = old_directional_gradients[:, coordinates]
-        new_laplacian = einsum(
-            hess, old_directional_gradients**2, "n ..., n d0 ... -> n ..."
-        ) + einsum(jac, old_laplacian, "n ..., n ... -> n ... ")
-    elif isinstance(layer, ReLU):
-        jac = gt(old_forward, 0).to(old_forward.dtype)
-        new_directional_gradients = einsum(
-            old_directional_gradients, jac, "n d0 ..., n ... -> n d0 ..."
-        )
+
+        # first-order contribution to Laplacian
         new_laplacian = einsum(jac, old_laplacian, "n ..., n ... -> n ... ")
-    elif isinstance(layer, Tanh):
-        jac = 1 - new_forward**2
-        hess = -2 * new_forward * jac
-        new_directional_gradients = einsum(
-            old_directional_gradients, jac, "n d0 ..., n ... -> n d0 ..."
-        )
-        # only use the relevant coordinates for the Laplacian
-        if coordinates is not None:
-            old_directional_gradients = old_directional_gradients[:, coordinates]
-        new_laplacian = einsum(
-            hess, old_directional_gradients**2, "n ..., n d0 ... -> n ..."
-        ) + einsum(jac, old_laplacian, "n ..., n ... -> n ... ")
+
+        # second-order contribution to Laplacian (zero for ReLU)
+        if hess is not None:
+            # only use the relevant coordinates for the Laplacian
+            coordinate_gradients = (
+                old_directional_gradients
+                if coordinates is None
+                else old_directional_gradients[:, coordinates]
+            )
+            if coefficients is None:
+                contribution = einsum(
+                    hess, coordinate_gradients**2, "n ..., n d0 ... -> n ..."
+                )
+            else:
+                contribution = einsum(
+                    hess,
+                    coordinate_gradients,
+                    coordinate_gradients,
+                    coefficients,
+                    "n ..., n i ..., n j ..., i j -> n ...",
+                )
+            new_laplacian.add_(contribution)
     else:
         raise NotImplementedError(f"Layer type not supported: {layer}.")
 
