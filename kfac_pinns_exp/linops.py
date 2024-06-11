1e79edb509d36c6783615bc14f98d83f98898e57
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-06-11 16:29:07 -0400

    [ADD] Support multiplication with per-layer Gramian

--- kfac_pinns_exp/linops.py
+++ kfac_pinns_exp/linops.py
@@ -20,7 +20,12 @@ class BoundaryGramianLinearOperator:
     backward pass. This saves computation when doing multiple Gramian-vector
     products, compared to matrix-free multiplication with the Gramian based on
     nested autodiff.
+
+    Attributes:
+        SUPPORTED_APPROXIMATIONS: The supported Gramian approximations.
     """
+
+    SUPPORTED_APPROXIMATIONS = {"full", "per_layer"}
 
     def __init__(
         self,
@@ -29,6 +34,7 @@ class BoundaryGramianLinearOperator:
         X: Tensor,
         y: Tensor,
         ggn_type: str = "type-2",
+        approximation: str = "full",
     ):
         """Pre-compute the information for the Gramian-vector product.
 
@@ -38,13 +44,22 @@ class BoundaryGramianLinearOperator:
             X: The input data tensor.
             y: The target data tensor.
             ggn_type: The type of GGN to use.
+            approximation: The Gramian approximation. Can be `'full'` or `'per_layer'`.
 
         Raises:
             NotImplementedError: If there are trainable parameters in unsupported
                 layers.
+            ValueError: For unsupported values of `approximation`.
         """
         self.layers = layers
         self.batch_size = X.shape[0]
+
+        if approximation not in self.SUPPORTED_APPROXIMATIONS:
+            raise ValueError(
+                f"Approximation '{approximation}' not supported. "
+                f"Choose from {self.SUPPORTED_APPROXIMATIONS}."
+            )
+        self.approximation = approximation
 
         self.layer_idxs = []
         for idx, layer in enumerate(layers):
@@ -106,30 +121,70 @@ class BoundaryGramianLinearOperator:
             v_p.reshape_as(p)
             for v_p, p in zip(v.split([p.numel() for p in self.params]), self.params)
         ]
-        JT_v = zeros(self.batch_size, device=v.device, dtype=v.dtype)
+
+        # matrix-vector product in list format
+        matmul_func = {"full": self._matmul_full, "per_layer": self._matmul_per_layer}[
+            self.approximation
+        ]
+        Gv_list = matmul_func(v_list)
+
+        # flatten and concatenate
+        return cat([Gv.flatten() for Gv in Gv_list])
+
+    def _matmul_full(self, v_list: List[Tensor]) -> List[Tensor]:
+        """Multiply the full Gramian onto a vector.
+
+        Args:
+            v_list: The vector to multiply with the Gramian in list format.
+
+        Returns:
+            The result of the Gramian-vector product in list format.
+        """
+        (dev,) = {v.device for v in v_list}
+        (dt,) = {v.dtype for v in v_list}
+        JT_v = zeros(self.batch_size, device=dev, dtype=dt)
 
         # multiply with the transpose Jacobian
         for i, layer_idx in enumerate(self.layer_idxs):
             z = self.layer_inputs[layer_idx]
             g = self.layer_grad_outputs[layer_idx]
-
+            # combine weight and bias
             v_idx = cat([v_list[2 * i], v_list[2 * i + 1].unsqueeze(-1)], dim=1)
-            JT_v.add_(
-                einsum(
-                    z, g, v_idx, "batch ... d_in, batch ... d_out, d_out d_in -> batch"
-                )
-            )
+            JT_v.add_(einsum(z, g, v_idx, "n ... d_in, n ... d_out, d_out d_in -> n"))
+
+        result = []
 
         # multiply with the Jacobian
+        for layer_idx in self.layer_idxs:
+            z = self.layer_inputs[layer_idx]
+            g = self.layer_grad_outputs[layer_idx]
+            v_idx = einsum(z, g, JT_v, "n ... d_in, n ... d_out, n -> d_out d_in")
+            # un-combine weight and bias
+            result.extend([v_idx[:, :-1], v_idx[:, -1]])
+
+        return result
+
+    def _matmul_per_layer(self, v_list: List[Tensor]) -> List[Tensor]:
+        """Multiply the per-layer Gramian onto a vector.
+
+        Args:
+            v: The vector to multiply with the Gramian in list format.
+
+        Returns:
+            The result of the per-layer Gramian-vector product in list format.
+        """
+        result = []
+
+        # multiply with the per-layer Gramian
         for i, layer_idx in enumerate(self.layer_idxs):
             z = self.layer_inputs[layer_idx]
             g = self.layer_grad_outputs[layer_idx]
+            # combine weight and bias
+            v_idx = cat([v_list[2 * i], v_list[2 * i + 1].unsqueeze(-1)], dim=1)
+            # multiply with JJT
+            JT_v = einsum(z, g, v_idx, "n ... d_in, n ... d_out, d_out d_in -> n")
+            v_idx = einsum(z, g, JT_v, "n ... d_in, n ... d_out, n -> d_out d_in")
+            # un-combine weight and bias
+            result.extend([v_idx[:, :-1], v_idx[:, -1]])
 
-            v_idx = einsum(
-                z, g, JT_v, "batch ... d_in, batch ... d_out, batch -> d_out d_in"
-            )
-            v_list[2 * i] = v_idx[:, :-1]
-            v_list[2 * i + 1] = v_idx[:, -1]
-
-        # flatten and concatenate
-        return cat([v.flatten() for v in v_list])
+        return result
--- test/test_linops.py
+++ test/test_linops.py
@@ -3,7 +3,7 @@
 from test.utils import report_nonclose
 
 from pytest import mark
-from torch import manual_seed, rand
+from torch import block_diag, manual_seed, rand
 from torch.nn import Linear, Sequential, Tanh
 
 from kfac_pinns_exp import heat_equation, poisson_equation
@@ -14,13 +14,18 @@ from kfac_pinns_exp.poisson_equation import square_boundary
 EQUATIONS = ["poisson", "heat"]
 EQUATION_IDS = [f"equation={eq}" for eq in EQUATIONS]
 
+APPROXIMATIONS = ["full", "per_layer"]
+APPROXIMATION_IDS = [f"approximation={approx}" for approx in APPROXIMATIONS]
+
 
 @mark.parametrize("equation", EQUATIONS, ids=EQUATION_IDS)
-def test_BoundaryGramianLinearOperator(equation: str):
+@mark.parametrize("approximation", APPROXIMATIONS, ids=APPROXIMATION_IDS)
+def test_BoundaryGramianLinearOperator(equation: str, approximation: str):
     """Check multiplication with the boundary Gramian via pre-computed quantities.
 
     Args:
         equation: A string specifying the PDE.
+        approximation: A string specifying the approximation of the Gramian.
     """
     manual_seed(0)
     dim_Omega = {"poisson": 2, "heat": 3}[equation]
@@ -40,12 +45,21 @@ def test_BoundaryGramianLinearOperator(equation: str):
     # autodiff
     param_names = [n for n, _ in model.named_parameters()]
     gramian = autograd_gramian(
-        model, X, param_names, loss_type=f"{equation}_boundary"
-    ).div_(N)
+        model,
+        X,
+        param_names,
+        loss_type=f"{equation}_boundary",
+        approximation=approximation,
+    )
+    if approximation == "per_layer":
+        gramian = block_diag(*gramian)
+    gramian.div_(N)
     Gv = gramian @ v
 
     # manual
-    G_linop = BoundaryGramianLinearOperator(equation, layers, X, y)
+    G_linop = BoundaryGramianLinearOperator(
+        equation, layers, X, y, approximation=approximation
+    )
     G_linop_v = G_linop @ v
 
     report_nonclose(Gv, G_linop_v)
