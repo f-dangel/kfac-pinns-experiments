c104d2bf8640eae24a7c2c6d78866d86e51a9495
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-06-12 09:15:44 -0400

    [ADD] Implement linear operator for interior Gramian

--- kfac_pinns_exp/linops.py
+++ kfac_pinns_exp/linops.py
@@ -188,3 +188,221 @@ class BoundaryGramianLinearOperator:
             result.extend([v_idx[:, :-1], v_idx[:, -1]])
 
         return result
+
+
+class InteriorGramianLinearOperator:
+    """Linear operator for the interior Gramian.
+
+    Pre-computes the information required to multiply with the Gramian in one
+    backward pass. This saves computation when doing multiple Gramian-vector
+    products, compared to matrix-free multiplication with the Gramian based on
+    nested autodiff.
+
+    Attributes:
+        SUPPORTED_APPROXIMATIONS: The supported Gramian approximations.
+    """
+
+    SUPPORTED_APPROXIMATIONS = {"full", "per_layer"}
+
+    def __init__(
+        self,
+        equation: str,
+        layers: List[Module],
+        X: Tensor,
+        y: Tensor,
+        ggn_type: str = "type-2",
+        approximation: str = "full",
+    ):
+        """Pre-compute the information for the Gramian-vector product.
+
+        Args:
+            equation: The type of equation to solve.
+            layers: The neural network's layers.
+            X: The input data tensor.
+            y: The target data tensor.
+            ggn_type: The type of GGN to use.
+            approximation: The Gramian approximation. Can be `'full'` or `'per_layer'`.
+
+        Raises:
+            NotImplementedError: If there are trainable parameters in unsupported
+                layers.
+            ValueError: For unsupported values of `approximation`.
+        """
+        self.layers = layers
+        self.batch_size = X.shape[0]
+
+        if approximation not in self.SUPPORTED_APPROXIMATIONS:
+            raise ValueError(
+                f"Approximation '{approximation}' not supported. "
+                f"Choose from {self.SUPPORTED_APPROXIMATIONS}."
+            )
+        self.approximation = approximation
+
+        self.layer_idxs = []
+        for idx, layer in enumerate(layers):
+            if isinstance(layer, Linear):
+                if (
+                    layer.weight.requires_grad
+                    and layer.bias is not None
+                    and layer.bias.requires_grad
+                ):
+                    self.layer_idxs.append(idx)
+                elif any(p.requires_grad for p in layer.parameters()):
+                    raise NotImplementedError(
+                        "Trainable linear layers must have differentiable weight+bias."
+                    )
+            elif any(p.requires_grad for p in layer.parameters()):
+                raise NotImplementedError(
+                    "Trainable parameters must be in linear layers."
+                )
+
+        self.params = sum(
+            (list(layers[idx].parameters()) for idx in self.layer_idxs), []
+        )
+        loss_evaluator = {
+            "poisson": poisson_equation.evaluate_interior_loss,
+            "heat": heat_equation.evaluate_interior_loss,
+        }[equation]
+        loss, residual, intermediates = loss_evaluator(layers, X, y)
+
+        # collect all layer output gradients
+        layer_outputs = []
+
+        for idx in self.layer_idxs:
+            layer_outputs.extend(
+                [
+                    intermediates[idx + 1]["forward"],
+                    intermediates[idx + 1]["directional_gradients"],
+                    intermediates[idx + 1]["laplacian"],
+                ]
+            )
+
+        error = get_backpropagated_error(residual, ggn_type).mul_(sqrt(self.batch_size))
+        grad_outputs = list(
+            grad(
+                residual,
+                layer_outputs,
+                grad_outputs=error,
+                retain_graph=True,
+                allow_unused=True,
+                materialize_grads=True,
+            )
+        )
+        self.layer_grad_outputs = {}
+        for idx in self.layer_idxs:
+            grad_forward = grad_outputs.pop(0)
+            grad_directional_gradients = grad_outputs.pop(0)
+            grad_laplacian = grad_outputs.pop(0)
+
+            g = cat(
+                [
+                    grad_forward.detach().unsqueeze(1),
+                    grad_directional_gradients.detach(),
+                    grad_laplacian.detach().unsqueeze(1),
+                ],
+                dim=1,
+            )
+            self.layer_grad_outputs[idx] = g
+
+        # collect all layer inputs
+        self.layer_inputs = {}
+        for idx in self.layer_idxs:
+            forward = intermediates[idx]["forward"]
+            directional_gradients = intermediates[idx]["directional_gradients"]
+            laplacian = intermediates[idx]["laplacian"]
+
+            z = cat(
+                [
+                    bias_augmentation(forward.detach(), 1).unsqueeze(1),
+                    bias_augmentation(directional_gradients.detach(), 0),
+                    bias_augmentation(laplacian.detach(), 0).unsqueeze(1),
+                ],
+                dim=1,
+            )
+            self.layer_inputs[idx] = z
+
+        # compute the Euclidean gradient
+        self.grad = grad(loss, self.params, allow_unused=True, materialize_grads=True)
+
+    def __matmul__(self, v: Tensor) -> Tensor:
+        """Multiply the Gramian onto a vector.
+
+        Args:
+            v: The vector to multiply with the Gramian. Has shape `[D]` where `D` is
+                the total number of parameters in the network.
+
+        Returns:
+            The result of the Gramian-vector product. Has shape `[D]`.
+        """
+        # split into parameters
+        v_list = [
+            v_p.reshape_as(p)
+            for v_p, p in zip(v.split([p.numel() for p in self.params]), self.params)
+        ]
+
+        # matrix-vector product in list format
+        matmul_func = {"full": self._matmul_full, "per_layer": self._matmul_per_layer}[
+            self.approximation
+        ]
+        Gv_list = matmul_func(v_list)
+
+        # flatten and concatenate
+        return cat([Gv.flatten() for Gv in Gv_list])
+
+    def _matmul_full(self, v_list: List[Tensor]) -> List[Tensor]:
+        """Multiply the full Gramian onto a vector.
+
+        Args:
+            v_list: The vector to multiply with the Gramian in list format.
+
+        Returns:
+            The result of the Gramian-vector product in list format.
+        """
+        (dev,) = {v.device for v in v_list}
+        (dt,) = {v.dtype for v in v_list}
+        JT_v = zeros(self.batch_size, device=dev, dtype=dt)
+
+        # multiply with the transpose Jacobian
+        for i, layer_idx in enumerate(self.layer_idxs):
+            z = self.layer_inputs[layer_idx]
+            g = self.layer_grad_outputs[layer_idx]
+            # combine weight and bias
+            v_idx = cat([v_list[2 * i], v_list[2 * i + 1].unsqueeze(-1)], dim=1)
+            JT_v.add_(einsum(z, g, v_idx, "n ... d_in, n ... d_out, d_out d_in -> n"))
+
+        result = []
+
+        # multiply with the Jacobian
+        for layer_idx in self.layer_idxs:
+            z = self.layer_inputs[layer_idx]
+            g = self.layer_grad_outputs[layer_idx]
+            v_idx = einsum(z, g, JT_v, "n ... d_in, n ... d_out, n -> d_out d_in")
+            # un-combine weight and bias
+            result.extend([v_idx[:, :-1], v_idx[:, -1]])
+
+        return result
+
+    def _matmul_per_layer(self, v_list: List[Tensor]) -> List[Tensor]:
+        """Multiply the per-layer Gramian onto a vector.
+
+        Args:
+            v: The vector to multiply with the Gramian in list format.
+
+        Returns:
+            The result of the per-layer Gramian-vector product in list format.
+        """
+        result = []
+
+        # multiply with the per-layer Gramian
+        for i, layer_idx in enumerate(self.layer_idxs):
+            z = self.layer_inputs[layer_idx]
+            g = self.layer_grad_outputs[layer_idx]
+            # combine weight and bias
+            v_idx = cat([v_list[2 * i], v_list[2 * i + 1].unsqueeze(-1)], dim=1)
+            # multiply with JJT
+            JT_v = einsum(z, g, v_idx, "n ... d_in, n ... d_out, d_out d_in -> n")
+            v_idx = einsum(z, g, JT_v, "n ... d_in, n ... d_out, n -> d_out d_in")
+            # un-combine weight and bias
+            result.extend([v_idx[:, :-1], v_idx[:, -1]])
+
+        return result
--- test/test_linops.py
+++ test/test_linops.py
@@ -6,10 +6,12 @@ from pytest import mark
 from torch import block_diag, manual_seed, rand
 from torch.nn import Linear, Sequential, Tanh
 
-from kfac_pinns_exp import heat_equation, poisson_equation
 from kfac_pinns_exp.autodiff_utils import autograd_gramian
-from kfac_pinns_exp.linops import BoundaryGramianLinearOperator
-from kfac_pinns_exp.poisson_equation import square_boundary
+from kfac_pinns_exp.linops import (
+    BoundaryGramianLinearOperator,
+    InteriorGramianLinearOperator,
+)
+from kfac_pinns_exp.train import create_condition_data, create_interior_data
 
 EQUATIONS = ["poisson", "heat"]
 EQUATION_IDS = [f"equation={eq}" for eq in EQUATIONS]
@@ -17,26 +19,35 @@ EQUATION_IDS = [f"equation={eq}" for eq in EQUATIONS]
 APPROXIMATIONS = ["full", "per_layer"]
 APPROXIMATION_IDS = [f"approximation={approx}" for approx in APPROXIMATIONS]
 
+LOSS_TYPES = ["boundary", "interior"]
+LOSS_TYPE_IDS = [f"loss_type={loss}" for loss in LOSS_TYPES]
+
 
 @mark.parametrize("equation", EQUATIONS, ids=EQUATION_IDS)
 @mark.parametrize("approximation", APPROXIMATIONS, ids=APPROXIMATION_IDS)
-def test_BoundaryGramianLinearOperator(equation: str, approximation: str):
-    """Check multiplication with the boundary Gramian via pre-computed quantities.
+@mark.parametrize("loss_type", LOSS_TYPES, ids=LOSS_TYPE_IDS)
+def test_GramianLinearOperator(equation: str, approximation: str, loss_type: str):
+    """Check multiplication with the Gramian via pre-computed quantities.
 
     Args:
         equation: A string specifying the PDE.
         approximation: A string specifying the approximation of the Gramian.
+        loss_type: A string specifying the type of loss.
     """
     manual_seed(0)
-    dim_Omega = {"poisson": 2, "heat": 3}[equation]
-    layers = [Linear(dim_Omega, 4), Tanh(), Linear(4, 3), Tanh(), Linear(3, 1)]
+    dim_Omega = 2
+    net_in_dim = {
+        "poisson": dim_Omega,
+        "heat": dim_Omega + 1,
+    }[equation]
+    layers = [Linear(net_in_dim, 4), Tanh(), Linear(4, 3), Tanh(), Linear(3, 1)]
     model = Sequential(*layers)
     N = 10
-    X = square_boundary(N, dim_Omega)
-    y = {
-        "poisson": poisson_equation.u_sin_product(X),
-        "heat": heat_equation.u_sin_product(X),
-    }[equation]
+    data_fn = {
+        "boundary": create_condition_data,
+        "interior": create_interior_data,
+    }[loss_type]
+    X, y = data_fn(equation, "sin_product", dim_Omega, N)
 
     # generate random vector
     num_params = sum(p.numel() for p in model.parameters())
@@ -48,7 +59,7 @@ def test_BoundaryGramianLinearOperator(equation: str, approximation: str):
         model,
         X,
         param_names,
-        loss_type=f"{equation}_boundary",
+        loss_type=f"{equation}_{loss_type}",
         approximation=approximation,
     )
     if approximation == "per_layer":
@@ -57,9 +68,11 @@ def test_BoundaryGramianLinearOperator(equation: str, approximation: str):
     Gv = gramian @ v
 
     # manual
-    G_linop = BoundaryGramianLinearOperator(
-        equation, layers, X, y, approximation=approximation
-    )
+    linop = {
+        "boundary": BoundaryGramianLinearOperator,
+        "interior": InteriorGramianLinearOperator,
+    }[loss_type]
+    G_linop = linop(equation, layers, X, y, approximation=approximation)
     G_linop_v = G_linop @ v
 
     report_nonclose(Gv, G_linop_v)
