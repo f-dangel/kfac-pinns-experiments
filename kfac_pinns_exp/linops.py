5b7c8e8ffa943dff0958c93fbe333bbe7543846a
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-06-12 09:31:45 -0400

    [REF] Extract multiplication routines into base class

--- kfac_pinns_exp/linops.py
+++ kfac_pinns_exp/linops.py
@@ -1,7 +1,7 @@
 """Implements linear operators."""
 
 from math import sqrt
-from typing import List
+from typing import Dict, List, Tuple
 
 from einops import einsum
 from torch import Tensor, cat, zeros
@@ -13,8 +13,8 @@ from kfac_pinns_exp.poisson_equation import get_backpropagated_error
 from kfac_pinns_exp.utils import bias_augmentation
 
 
-class BoundaryGramianLinearOperator:
-    """Linear operator for the boundary Gramian.
+class _GramianLinearOperator:
+    """Base class for linear operators representing a Gramian.
 
     Pre-computes the information required to multiply with the Gramian in one
     backward pass. This saves computation when doing multiple Gramian-vector
@@ -53,6 +53,8 @@ class BoundaryGramianLinearOperator:
         """
         self.layers = layers
         self.batch_size = X.shape[0]
+        self.ggn_type = ggn_type
+        self.equation = equation
 
         if approximation not in self.SUPPORTED_APPROXIMATIONS:
             raise ValueError(
@@ -82,29 +84,29 @@ class BoundaryGramianLinearOperator:
         self.params = sum(
             (list(layers[idx].parameters()) for idx in self.layer_idxs), []
         )
-        loss_evaluator = {
-            "poisson": poisson_equation.evaluate_boundary_loss,
-            "heat": heat_equation.evaluate_boundary_loss,
-        }[equation]
-        loss, residual, intermediates = loss_evaluator(layers, X, y)
-
-        # collect all layer output gradients
-        layer_outputs = [intermediates[idx + 1] for idx in self.layer_idxs]
-        error = get_backpropagated_error(residual, ggn_type).mul_(sqrt(self.batch_size))
-        grad_outputs = grad(
-            residual, layer_outputs, grad_outputs=error, retain_graph=True
+        # compute quantities required for Gramian-vector products
+        loss, self.layer_inputs, self.layer_grad_outputs = (
+            self.compute_layer_inputs_and_grad_outputs(X, y)
         )
-        self.layer_grad_outputs = {
-            idx: g for g, idx in zip(grad_outputs, self.layer_idxs)
-        }
+        self.grad = grad(loss, self.params, allow_unused=True, materialize_grads=True)
 
-        # collect all layer inputs
-        self.layer_inputs = {
-            idx: bias_augmentation(intermediates[idx], 1) for idx in self.layer_idxs
-        }
+    def compute_layer_inputs_and_grad_outputs(
+        self, X: Tensor, y: Tensor
+    ) -> Tuple[Tensor, Dict[int, Tensor], Dict[int, Tensor]]:
+        """Pre-compute loss as well as inputs and output gradients of supported layers.
 
-        # compute the Euclidean gradient
-        self.grad = grad(loss, self.params)
+        Args:
+            X: The input data tensor.
+            y: The target data tensor.
+
+        Returns:
+            The loss, and dictionaries of layer inputs and output gradients.
+            Keys are indices of the layer.
+
+        Raises:
+            NotImplementedError: Must be implemented by a subclass.
+        """
+        raise NotImplementedError
 
     def __matmul__(self, v: Tensor) -> Tensor:
         """Multiply the Gramian onto a vector.
@@ -190,80 +192,67 @@ class BoundaryGramianLinearOperator:
         return result
 
 
-class InteriorGramianLinearOperator:
-    """Linear operator for the interior Gramian.
+class BoundaryGramianLinearOperator(_GramianLinearOperator):
+    """Linear operator for a boundary Gramian."""
 
-    Pre-computes the information required to multiply with the Gramian in one
-    backward pass. This saves computation when doing multiple Gramian-vector
-    products, compared to matrix-free multiplication with the Gramian based on
-    nested autodiff.
-
-    Attributes:
-        SUPPORTED_APPROXIMATIONS: The supported Gramian approximations.
-    """
-
-    SUPPORTED_APPROXIMATIONS = {"full", "per_layer"}
-
-    def __init__(
-        self,
-        equation: str,
-        layers: List[Module],
-        X: Tensor,
-        y: Tensor,
-        ggn_type: str = "type-2",
-        approximation: str = "full",
-    ):
-        """Pre-compute the information for the Gramian-vector product.
+    def compute_layer_inputs_and_grad_outputs(
+        self, X: Tensor, y: Tensor
+    ) -> Tuple[Tensor, Dict[int, Tensor], Dict[int, Tensor]]:
+        """Pre-compute loss as well as inputs and output gradients of supported layers.
 
         Args:
-            equation: The type of equation to solve.
-            layers: The neural network's layers.
             X: The input data tensor.
             y: The target data tensor.
-            ggn_type: The type of GGN to use.
-            approximation: The Gramian approximation. Can be `'full'` or `'per_layer'`.
 
-        Raises:
-            NotImplementedError: If there are trainable parameters in unsupported
-                layers.
-            ValueError: For unsupported values of `approximation`.
+        Returns:
+            The loss, and dictionaries of layer inputs and output gradients.
+            Keys are indices of the layer.
         """
-        self.layers = layers
-        self.batch_size = X.shape[0]
+        loss_evaluator = {
+            "poisson": poisson_equation.evaluate_boundary_loss,
+            "heat": heat_equation.evaluate_boundary_loss,
+        }[self.equation]
+        loss, residual, intermediates = loss_evaluator(self.layers, X, y)
 
-        if approximation not in self.SUPPORTED_APPROXIMATIONS:
-            raise ValueError(
-                f"Approximation '{approximation}' not supported. "
-                f"Choose from {self.SUPPORTED_APPROXIMATIONS}."
-            )
-        self.approximation = approximation
-
-        self.layer_idxs = []
-        for idx, layer in enumerate(layers):
-            if isinstance(layer, Linear):
-                if (
-                    layer.weight.requires_grad
-                    and layer.bias is not None
-                    and layer.bias.requires_grad
-                ):
-                    self.layer_idxs.append(idx)
-                elif any(p.requires_grad for p in layer.parameters()):
-                    raise NotImplementedError(
-                        "Trainable linear layers must have differentiable weight+bias."
-                    )
-            elif any(p.requires_grad for p in layer.parameters()):
-                raise NotImplementedError(
-                    "Trainable parameters must be in linear layers."
-                )
-
-        self.params = sum(
-            (list(layers[idx].parameters()) for idx in self.layer_idxs), []
+        # collect all layer output gradients
+        layer_outputs = [intermediates[idx + 1] for idx in self.layer_idxs]
+        error = get_backpropagated_error(residual, self.ggn_type).mul_(
+            sqrt(self.batch_size)
         )
+        grad_outputs = grad(
+            residual, layer_outputs, grad_outputs=error, retain_graph=True
+        )
+        layer_grad_outputs = {idx: g for g, idx in zip(grad_outputs, self.layer_idxs)}
+
+        # collect all layer inputs
+        layer_inputs = {
+            idx: bias_augmentation(intermediates[idx], 1) for idx in self.layer_idxs
+        }
+
+        return loss, layer_inputs, layer_grad_outputs
+
+
+class InteriorGramianLinearOperator(_GramianLinearOperator):
+    """Linear operator for an interior Gramian."""
+
+    def compute_layer_inputs_and_grad_outputs(
+        self, X: Tensor, y: Tensor
+    ) -> Tuple[Tensor, Dict[int, Tensor], Dict[int, Tensor]]:
+        """Pre-compute loss as well as inputs and output gradients of supported layers.
+
+        Args:
+            X: The input data tensor.
+            y: The target data tensor.
+
+        Returns:
+            The loss, and dictionaries of layer inputs and output gradients.
+            Keys are indices of the layer.
+        """
         loss_evaluator = {
             "poisson": poisson_equation.evaluate_interior_loss,
             "heat": heat_equation.evaluate_interior_loss,
-        }[equation]
-        loss, residual, intermediates = loss_evaluator(layers, X, y)
+        }[self.equation]
+        loss, residual, intermediates = loss_evaluator(self.layers, X, y)
 
         # collect all layer output gradients
         layer_outputs = []
@@ -277,7 +266,9 @@ class InteriorGramianLinearOperator:
                 ]
             )
 
-        error = get_backpropagated_error(residual, ggn_type).mul_(sqrt(self.batch_size))
+        error = get_backpropagated_error(residual, self.ggn_type).mul_(
+            sqrt(self.batch_size)
+        )
         grad_outputs = list(
             grad(
                 residual,
@@ -288,7 +279,7 @@ class InteriorGramianLinearOperator:
                 materialize_grads=True,
             )
         )
-        self.layer_grad_outputs = {}
+        layer_grad_outputs = {}
         for idx in self.layer_idxs:
             grad_forward = grad_outputs.pop(0)
             grad_directional_gradients = grad_outputs.pop(0)
@@ -302,10 +293,10 @@ class InteriorGramianLinearOperator:
                 ],
                 dim=1,
             )
-            self.layer_grad_outputs[idx] = g
+            layer_grad_outputs[idx] = g
 
         # collect all layer inputs
-        self.layer_inputs = {}
+        layer_inputs = {}
         for idx in self.layer_idxs:
             forward = intermediates[idx]["forward"]
             directional_gradients = intermediates[idx]["directional_gradients"]
@@ -319,90 +310,6 @@ class InteriorGramianLinearOperator:
                 ],
                 dim=1,
             )
-            self.layer_inputs[idx] = z
+            layer_inputs[idx] = z
 
-        # compute the Euclidean gradient
-        self.grad = grad(loss, self.params, allow_unused=True, materialize_grads=True)
-
-    def __matmul__(self, v: Tensor) -> Tensor:
-        """Multiply the Gramian onto a vector.
-
-        Args:
-            v: The vector to multiply with the Gramian. Has shape `[D]` where `D` is
-                the total number of parameters in the network.
-
-        Returns:
-            The result of the Gramian-vector product. Has shape `[D]`.
-        """
-        # split into parameters
-        v_list = [
-            v_p.reshape_as(p)
-            for v_p, p in zip(v.split([p.numel() for p in self.params]), self.params)
-        ]
-
-        # matrix-vector product in list format
-        matmul_func = {"full": self._matmul_full, "per_layer": self._matmul_per_layer}[
-            self.approximation
-        ]
-        Gv_list = matmul_func(v_list)
-
-        # flatten and concatenate
-        return cat([Gv.flatten() for Gv in Gv_list])
-
-    def _matmul_full(self, v_list: List[Tensor]) -> List[Tensor]:
-        """Multiply the full Gramian onto a vector.
-
-        Args:
-            v_list: The vector to multiply with the Gramian in list format.
-
-        Returns:
-            The result of the Gramian-vector product in list format.
-        """
-        (dev,) = {v.device for v in v_list}
-        (dt,) = {v.dtype for v in v_list}
-        JT_v = zeros(self.batch_size, device=dev, dtype=dt)
-
-        # multiply with the transpose Jacobian
-        for i, layer_idx in enumerate(self.layer_idxs):
-            z = self.layer_inputs[layer_idx]
-            g = self.layer_grad_outputs[layer_idx]
-            # combine weight and bias
-            v_idx = cat([v_list[2 * i], v_list[2 * i + 1].unsqueeze(-1)], dim=1)
-            JT_v.add_(einsum(z, g, v_idx, "n ... d_in, n ... d_out, d_out d_in -> n"))
-
-        result = []
-
-        # multiply with the Jacobian
-        for layer_idx in self.layer_idxs:
-            z = self.layer_inputs[layer_idx]
-            g = self.layer_grad_outputs[layer_idx]
-            v_idx = einsum(z, g, JT_v, "n ... d_in, n ... d_out, n -> d_out d_in")
-            # un-combine weight and bias
-            result.extend([v_idx[:, :-1], v_idx[:, -1]])
-
-        return result
-
-    def _matmul_per_layer(self, v_list: List[Tensor]) -> List[Tensor]:
-        """Multiply the per-layer Gramian onto a vector.
-
-        Args:
-            v: The vector to multiply with the Gramian in list format.
-
-        Returns:
-            The result of the per-layer Gramian-vector product in list format.
-        """
-        result = []
-
-        # multiply with the per-layer Gramian
-        for i, layer_idx in enumerate(self.layer_idxs):
-            z = self.layer_inputs[layer_idx]
-            g = self.layer_grad_outputs[layer_idx]
-            # combine weight and bias
-            v_idx = cat([v_list[2 * i], v_list[2 * i + 1].unsqueeze(-1)], dim=1)
-            # multiply with JJT
-            JT_v = einsum(z, g, v_idx, "n ... d_in, n ... d_out, d_out d_in -> n")
-            v_idx = einsum(z, g, JT_v, "n ... d_in, n ... d_out, n -> d_out d_in")
-            # un-combine weight and bias
-            result.extend([v_idx[:, :-1], v_idx[:, -1]])
-
-        return result
+        return loss, layer_inputs, layer_grad_outputs
