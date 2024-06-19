a690806cfec7230b3391bf6f9ebb4ad6924e95ff
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-06-19 15:48:46 -0400

    [ADD] Finish implementing FP boundary+interior loss [no ci]

--- kfac_pinns_exp/fokker_planck_equation.py
+++ kfac_pinns_exp/fokker_planck_equation.py
@@ -2,13 +2,13 @@
 
 from typing import Callable, Dict, List, Tuple, Union
 
-from torch import Tensor, cat, tensor, zeros
-from torch.autograd import grad
+from einops import einsum
+from torch import Tensor, cat, tensor
 from torch.nn import Module
 
 from kfac_pinns_exp.autodiff_utils import (
+    autograd_input_divergence,
     autograd_input_hessian,
-    autograd_input_jacobian,
 )
 
 
@@ -36,66 +36,49 @@ def evaluate_interior_loss(
         of the computation graph that can be used to compute (approximate) curvature.
 
     Raises:
-        ValueError: If the model is not a Module or a list of Modules.
+        ValueError: If the model is not a PyTorch `Module`.
     """
-    # use autograd to compute the Laplacian
-    if isinstance(model, Module):
-        # X[0] = (t, x_1, x_2 , ..., x_d)
-        intermediates = None
-
-        hessian_p = autograd_input_hessian(model, X)
-        jacobian_p = autograd_input_jacobian(model, X)
-        jacobian_p_t = jacobian_p[:, 0]
-        # jacobian_p_x = jacobian_p[:, 1:]
-
-        # [batch_size, d]
-        X_spatial = X[:, 1:].requires_grad_()
-        # [batch_size, 1]
-        t = X[:, [0]].requires_grad_()
-
-        # [batch_size, d + 1]
-        X = cat((t, X_spatial), dim=1)
-
-        # [batch_size, 1]
-        p = model(X)
-
-        # [batch_size, d]
-        mu_t = mu(t)  # mu_t[0] = (mu_1, ..., mu_d)
-        p_mu = p * mu_t
-
-        batch_size, D = p_mu.shape
-
-        div = zeros(batch_size, 1)
-
-        for n in range(batch_size):
-            for d in range(D):
-                # [batch_size, d]
-                # ∂p_mu[n, d] / ∂x_d
-                (g,) = grad(p_mu[n, d], X_spatial, retain_graph=True)
-                div[n].add_(g[n, d])
-
-        sigma_t = sigma(t)
-
-        # t.requires_grad_()
-
-        # batch_size, d = t.shape[1]
-        # for n in range(batch_size):
-        #     jacobian_mu_n_t =  grad(mu_t, t)
-
-        # laplacian = einsum(input_hessian, "batch i i -> batch").unsqueeze(-1)
-    else:
+    if not isinstance(model, Module):
         raise NotImplementedError
-    # # use the forward Laplacian framework
-    # elif isinstance(model, list) and all(isinstance(layer, Module) for layer in model):
-    #     intermediates = manual_forward_laplacian(model, X)
-    #     laplacian = intermediates[-1]["laplacian"]
-    # else:
-    #     raise ValueError(
-    #         "Model must be a Module or a list of Modules that form a sequential model."
-    #         f"Got: {model}."
-    #     )
-    # residual = laplacian + y
-    # return 0.5 * (residual**2).mean(), residual, intermediates
+
+    # one datum X[n] has coordinates (t, x_1, x_2 , ..., x_d)
+
+    # split time and spatial coordinates
+    spatial = X[:, 1:].requires_grad_()  # [batch_size, d]
+    t = X[:, [0]].requires_grad_()  # [batch_size, 1]
+    X = cat((t, spatial), dim=1)  # [batch_size, d + 1]
+
+    # compute ∂p/∂t + div(p μ) = div[ p(t, x) (1, μ(t, x))ᵀ ]
+    def p_times_mu(x: Tensor) -> Tensor:
+        """Compute the product between p(t, x) and the augmented μ(t, x).
+
+        Args:
+            x: Un-batched input (time and spatial coordinates).
+
+        Returns:
+            Product of p(t, x) and augmented μ(t, x).
+        """
+        mu_x = mu(x)
+        mu_x_augmented = cat([tensor(1.0).expand_as(mu_x), mu_x], dim=0)
+        return model(x) * mu_x_augmented
+
+    dp_dt_plus_div_p_times_mu = autograd_input_divergence(p_times_mu, X)
+
+    # compute 1/2 Tr(σ σᵀ ∂²p/∂x²)
+    hessian_X = autograd_input_hessian(model, X)  # [batch_size, d + 1, d + 1]
+    hessian_spatial = hessian_X[:, 1:, 1:]  # [batch_size, d, d]
+
+    sigma_X = sigma(X)
+    sigma_outer = einsum(sigma_X, sigma_X, "batch i j, batch k j -> batch i k")
+    sigma_outer_hessian = einsum(
+        sigma_outer, hessian_spatial, "batch i k, batch k j -> batch i j"
+    )
+    tr_sigma_outer_hessian = einsum(sigma_outer_hessian, "batch i i -> batch")
+
+    # compute residual and loss
+    residual = (dp_dt_plus_div_p_times_mu - 0.5 * tr_sigma_outer_hessian) - y
+    loss = 0.5 * (residual**2).mean()
+    return loss, residual, None
 
 
 def evaluate_boundary_loss(
@@ -114,19 +97,13 @@ def evaluate_boundary_loss(
         curvature.
 
     Raises:
-        ValueError: If the model is not a Module or a list of Modules.
+        NotImplementedError: If the model is not a PyTorch `Module`.
     """
-    raise NotImplementedError
-    # if isinstance(model, Module):
-    #     output = model(X)
-    #     intermediates = None
-    # elif isinstance(model, list) and all(isinstance(layer, Module) for layer in model):
-    #     intermediates = manual_forward(model, X)
-    #     output = intermediates[-1]
-    # else:
-    #     raise ValueError(
-    #         "Model must be a Module or a list of Modules that form a sequential model."
-    #         f"Got: {model}."
-    #     )
-    # residual = output - y
-    # return 0.5 * (residual**2).mean(), residual, intermediates
+    if not isinstance(model, Module):
+        raise NotImplementedError
+
+    output = model(X)
+    intermediates = None
+    residual = output - y
+
+    return 0.5 * (residual**2).mean(), residual, intermediates
