aa918708ca4d508ceeb84d712fcaeb1fb1725124
Felix Dangel <48687646+f-dangel@users.noreply.github.com>
2024-07-10 10:43:40 -0400

    [ADD] Prototype Fokker-Planck equation in log space (#93)
    * [ADD] Prototype of Fokker-Planck equation in log space [no ci]
    
    * [FIX] Shape error, add test cases for log-fokker-planck
    
    * [REF] Fix linters
    
    * [REF] More linter fixes

---
+++ kfac_pinns_exp/log_fokker_planck_isotropic_equation.py
@@ -0,0 +1,21 @@
+"""Diffusivity, vector fields, and solutions of an isotropic Fokker-Planck equation."""
+
+from torch import Tensor
+
+from kfac_pinns_exp import fokker_planck_isotropic_equation
+
+mu_isotropic = fokker_planck_isotropic_equation.mu_isotropic
+sigma_isotropic = fokker_planck_isotropic_equation.sigma_isotropic
+
+
+def q_isotropic_gaussian(X: Tensor) -> Tensor:
+    """Isotropic Gaussian solution to the Fokker-Planck equation in log-space.
+
+    Args:
+        X: Batched quadrature points of shape `(N, d_Omega + 1)`.
+
+    Returns:
+        The function values as tensor of shape `(N, 1)`.
+    """
+    # TODO Implement this in log space to avoid numerical instabilities
+    return fokker_planck_isotropic_equation.p_isotropic_gaussian(X).log()
