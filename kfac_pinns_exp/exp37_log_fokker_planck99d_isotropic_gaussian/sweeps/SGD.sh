6041206e70479f96f858a1b483c46b78ff2903f7
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-07-16 16:43:00 -0400

    [ADD] Sweeps for 9/99d log-FP equations [no ci]

---
+++ kfac_pinns_exp/exp37_log_fokker_planck99d_isotropic_gaussian/sweeps/SGD.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --partition=rtx6000
+#SBATCH --qos=m3
+
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=04:00:00
+#SBATCH --mem-per-cpu=8G
+#SBATCH --array=1-64%1
+
+echo "[DEBUG] Host name: " `hostname`
+
+source  ~/anaconda3/etc/profile.d/conda.sh
+conda activate kfac_pinns_exp
+
+wandb agent --count 1 kfac-pinns/log_fokker_planck99d_isotropic_gaussian_bayes/3f6e5wyk
