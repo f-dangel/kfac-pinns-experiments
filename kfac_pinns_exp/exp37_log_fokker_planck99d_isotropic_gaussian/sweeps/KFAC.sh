3f1f360967da0005f0e8ada4338b3773505de799
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-07-17 09:37:54 -0400

    [REF] Reduce max batch size for 99d log-FP [no ci]

--- kfac_pinns_exp/exp37_log_fokker_planck99d_isotropic_gaussian/sweeps/KFAC.sh
+++ kfac_pinns_exp/exp37_log_fokker_planck99d_isotropic_gaussian/sweeps/KFAC.sh
@@ -13,4 +13,4 @@ echo "[DEBUG] Host name: " `hostname`
 source  ~/anaconda3/etc/profile.d/conda.sh
 conda activate kfac_pinns_exp
 
-wandb agent --count 1 kfac-pinns/log_fokker_planck99d_isotropic_gaussian_bayes/jtuwqyqo
+wandb agent --count 1 kfac-pinns/log_fokker_planck99d_isotropic_gaussian_bayes/faox2ifq
--- kfac_pinns_exp/exp37_log_fokker_planck99d_isotropic_gaussian/sweeps/KFAC_auto.sh
+++ kfac_pinns_exp/exp37_log_fokker_planck99d_isotropic_gaussian/sweeps/KFAC_auto.sh
@@ -13,4 +13,4 @@ echo "[DEBUG] Host name: " `hostname`
 source  ~/anaconda3/etc/profile.d/conda.sh
 conda activate kfac_pinns_exp
 
-wandb agent --count 1 kfac-pinns/log_fokker_planck99d_isotropic_gaussian_bayes/u30fm2dv
+wandb agent --count 1 kfac-pinns/log_fokker_planck99d_isotropic_gaussian_bayes/3zt3ziz5
