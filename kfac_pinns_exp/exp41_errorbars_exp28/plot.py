e524e3accdeeff1fccf4b1d232a482265f1c51c7
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-08-07 19:02:32 +0200

    [ADD] Robustness plots under model variation

--- kfac_pinns_exp/exp41_errorbars_exp28/plot.py
+++ kfac_pinns_exp/exp41_errorbars_exp28/plot.py
@@ -13,8 +13,9 @@ from kfac_pinns_exp.exp28_heat4d_medium.plot import (
     num_params,
 )
 from kfac_pinns_exp.exp41_errorbars_exp28.create_launch_script import (
-    COMMANDS,
     REPEATDIR,
+    VARY_DATA_COMMANDS,
+    VARY_MODEL_COMMANDS,
     entity,
     project,
     sweep_ids,
@@ -23,36 +24,36 @@ from kfac_pinns_exp.wandb_utils import download_run
 
 HEREDIR = path.dirname(path.abspath(__file__))
 
-
 y_to_ylabel = {"loss": "Loss", "l2_error": "$L_2$ error"}
-x_to_xlabel = {"step": "Iteration", "time": "Time (s)"}
+x_to_xlabel = {"step": "Iteration", "time": "Time [s]"}
 
-x = "step"
-y = "loss"
+VARIATIONS = {
+    "model": VARY_MODEL_COMMANDS,
+    "data": VARY_DATA_COMMANDS,
+}
 
-for (x, xlabel), (y, ylabel) in product(x_to_xlabel.items(), y_to_ylabel.items()):
-    with plt.rc_context(bundles.neurips2023(rel_width=1.0)):
-        fig, ax = plt.subplots(1, 1)
-        ax.set_xlabel(xlabel)
-        ax.set_xscale("log")
-        ax.set_ylabel(ylabel)
-        ax.set_yscale("log")
-        ax.set_title(f"4d Heat ({architecture}, $D={num_params}$)")
-        ax.grid(True, alpha=0.5)
+for variation, commands in VARIATIONS.items():
+    for plot_idx, ((x, xlabel), (y, ylabel)) in enumerate(
+        product(x_to_xlabel.items(), y_to_ylabel.items())
+    ):
+        with plt.rc_context(bundles.neurips2023(rel_width=1.0)):
+            fig, ax = plt.subplots(1, 1)
+            ax.set_xlabel(xlabel)
+            ax.set_xscale("log")
+            ax.set_ylabel(ylabel)
+            ax.set_yscale("log")
+            ax.set_title(f"4d Heat ({architecture}, $D={num_params}$)")
+            ax.grid(True, alpha=0.5)
 
-        from wandb.errors import CommError
-
-        for sweep_id, best_runs in COMMANDS.items():
-            for idx, run_id in enumerate(best_runs.keys()):
-                if idx >= 1:
-                    continue
-                try:
+            for sweep_id, best_runs in commands.items():
+                for idx, run_id in enumerate(best_runs.keys()):
                     df_history, _ = download_run(
                         entity,
                         project,
                         run_id,
                         savedir=REPEATDIR,
-                        # update=False,
+                        # only download data again for first sub-plot, then re-use
+                        update=plot_idx == 0,
                     )
                     label = sweep_ids[sweep_id]
 
@@ -68,12 +69,15 @@ for (x, xlabel), (y, ylabel) in product(x_to_xlabel.items(), y_to_ylabel.items()
                         color=colors[label],
                         linestyle=linestyles[label],
                     )
-                except CommError:
-                    pass
 
-        # set y max to 10
-        ymin, ymax = ax.get_ylim()
-        ax.set_ylim(ymin, min(ymax, 10))
+            ax.legend()
 
-        ax.legend()
-        plt.savefig(path.join(HEREDIR, f"{y}_over_{x}.pdf"), bbox_inches="tight")
+            # set y max to 10 because LBFGS sometimes diverges
+            # also need to set the bottom value because otherwise it is way too small
+            bottom = {"loss": 1e-11, "l2_error": 5e-6}[y]
+            plt.gca().set_ylim(bottom=bottom, top=1e1)
+
+            plt.savefig(
+                path.join(HEREDIR, f"{y}_over_{x}_vary_{variation}.pdf"),
+                bbox_inches="tight",
+            )
