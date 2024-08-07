f8a984beed12b0f8aa8aae5f42987881aadb7a1d
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-08-07 14:11:50 +0200

    [ADD] Script to generate launch script to repeat best runs

---
+++ kfac_pinns_exp/exp28_heat4d_medium/repeat_best_runs.py
@@ -0,0 +1,97 @@
+"""Re-run best hyperparameters for different seeds."""
+
+from argparse import ArgumentParser
+from os import makedirs, path
+
+from kfac_pinns_exp.exp09_reproduce_poisson2d.yaml_to_sh import QUEUE_TO_TIME
+from kfac_pinns_exp.exp28_heat4d_medium.plot import DATADIR, entity, project, sweep_ids
+from kfac_pinns_exp.wandb_utils import load_best_run
+
+HEREDIR = path.dirname(path.abspath(__file__))
+REPEATDIR = path.join(HEREDIR, "repeated_runs")
+makedirs(REPEATDIR, exist_ok=True)
+
+parser = ArgumentParser(description="Plot the best runs from each tuned optimizer.")
+parser.add_argument(
+    "--local_files",
+    action="store_false",
+    dest="update",
+    help="Use local files if possible.",
+    default=True,
+)
+args = parser.parse_args()
+
+commands = {sweep_id: {} for sweep_id in sweep_ids.keys()}
+
+for sweep_id, label in sweep_ids.items():
+    # load meta-data of the run
+    _, df_meta = load_best_run(
+        entity,
+        project,
+        sweep_id,
+        save=True,
+        update=args.update,
+        savedir=DATADIR,
+    )
+    df_meta = df_meta.to_dict()
+    run_name = df_meta["name"][0]
+    run_cmd = df_meta["config"][0]["cmd"].split(" ")
+
+    # drop random seed from run_cmd
+    run_cmd = [arg for arg in run_cmd if "--model_seed" not in arg]
+
+    # fill dictionary with commands to run
+    for s in range(1, 11):
+        run_id = f"{run_name}_model_seed_{s}"
+        commands[sweep_id][run_id] = " ".join(
+            run_cmd
+            + [
+                f"--model_seed={s}",
+                f"--wandb_entity={entity}",
+                f"--wandb_project={project}",
+                f"--wandb_id={run_id}",
+            ]
+        )
+
+TEMPLATE = r"""#!/bin/bash
+#SBATCH --partition=PARTITION_PLACEHOLDER
+#SBATCH --qos=QOS_PLACEHOLDER
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=TIME_PLACEHOLDER
+#SBATCH --mem-per-cpu=8G
+#SBATCH --array=0-ARRAY_PLACEHOLDER
+
+echo "[DEBUG] Host name: " `hostname`
+
+JOBS=(
+JOBS_PLACEHOLDER
+)
+
+CMD=${JOBS[$SLURM_ARRAY_TASK_ID]}
+
+echo 'Running $CMD'
+$CMD
+"""
+
+
+if __name__ == "__main__":
+    # write the launch script
+    cmds_flat = []
+    for sweep_commands in commands.values():
+        cmds_flat.extend(iter(sweep_commands.values()))
+
+    jobs = [f"{cmd!r}" for cmd in cmds_flat]
+    script = TEMPLATE.replace("JOBS_PLACEHOLDER", "\t" + "\n\t".join(jobs))
+    script = script.replace("ARRAY_PLACEHOLDER", str(len(jobs) - 1))
+
+    partition = "rtx6000"
+    script = script.replace("PARTITION_PLACEHOLDER", partition)
+
+    qos = "m5"
+    time = QUEUE_TO_TIME[qos]
+    script = script.replace("QOS_PLACEHOLDER", qos)
+    script = script.replace("TIME_PLACEHOLDER", time)
+
+    with open(path.join(REPEATDIR, "launch.sh"), "w") as f:
+        f.write(script)
