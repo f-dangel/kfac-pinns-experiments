46a6bae4e05c8e80aa9e27b8e1bb5b1d93a91dc7
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-08-08 19:11:15 +0200

    [REF] Even earlier phase of visualization

--- kfac_pinns_exp/exp42_visualize_solutions/visualize_solution/launch.sh
+++ kfac_pinns_exp/exp42_visualize_solutions/visualize_solution/launch.sh
@@ -10,15 +10,15 @@
 echo "[DEBUG] Host name: " `hostname`
 
 JOBS=(
-	'python ../../train.py --optimizer=SGD --SGD_lr=0.018050148049887416 --SGD_momentum=0.99 --num_seconds=100 --plot_solution --plot_dir=SGD --plot_steps 0 335 653 1052 1400'
-	'python ../../train.py --optimizer=Adam --Adam_lr=0.0016923389183852923 --num_seconds=100 --plot_solution --plot_dir=Adam --plot_steps 0 335 653 956 1272'
-	'python ../../train.py --optimizer=HessianFree --HessianFree_cg_max_iter=50 --HessianFree_curvature_opt=ggn --HessianFree_damping=100 --num_seconds=100 --plot_solution --plot_dir=Hessian-free --plot_steps 0 4 7 11 14'
-	'python ../../train.py --optimizer=LBFGS --LBFGS_history_size=150 --LBFGS_lr=0.5 --num_seconds=100 --plot_solution --plot_dir=LBFGS --plot_steps 0 50 98 143 190'
-	'python ../../train.py --optimizer=ENGD --ENGD_approximation=full --ENGD_damping=1e-10 --ENGD_ema_factor=0.3 --ENGD_initialize_to_identity --num_seconds=100 --plot_solution --plot_dir=ENGD_full --plot_steps 0 6 11 17 23'
-	'python ../../train.py --optimizer=ENGD --ENGD_approximation=per_layer --ENGD_damping=0 --ENGD_ema_factor=0.9 --ENGD_initialize_to_identity --num_seconds=100 --plot_solution --plot_dir=ENGD_layer-wise --plot_steps 0 6 12 18 23'
-	'python ../../train.py --optimizer=ENGD --ENGD_approximation=diagonal --ENGD_damping=0.0001 --ENGD_ema_factor=0.3 --num_seconds=100 --plot_solution --plot_dir=ENGD_diagonal --plot_steps 0 7 13 20 27'
-	'python ../../train.py --optimizer=KFAC --KFAC_damping=1.5440986717145857e-12 --KFAC_ema_factor=0.4496490467123582 --KFAC_initialize_to_identity --KFAC_momentum=0.5117574706015794 --num_seconds=100 --plot_solution --plot_dir=KFAC --plot_steps 0 20 40 61 81'
-	'python ../../train.py --optimizer=KFAC --KFAC_damping=1.2156399458704562e-10 --KFAC_ema_factor=0.9263313896660544 --KFAC_initialize_to_identity --KFAC_lr=auto --num_seconds=100 --plot_solution --plot_dir=KFAC_auto --plot_steps 0 35 67 107 143'
+	'python ../../train.py --optimizer=SGD --SGD_lr=0.018050148049887416 --SGD_momentum=0.99 --num_seconds=100 --plot_solution --plot_dir=SGD --plot_steps 172 335 491 653'
+	'python ../../train.py --optimizer=Adam --Adam_lr=0.0016923389183852923 --num_seconds=100 --plot_solution --plot_dir=Adam --plot_steps 157 335 491 653'
+	'python ../../train.py --optimizer=HessianFree --HessianFree_cg_max_iter=50 --HessianFree_curvature_opt=ggn --HessianFree_damping=100 --num_seconds=100 --plot_solution --plot_dir=Hessian-free --plot_steps 2 4 5 7'
+	'python ../../train.py --optimizer=LBFGS --LBFGS_history_size=150 --LBFGS_lr=0.5 --num_seconds=100 --plot_solution --plot_dir=LBFGS --plot_steps 25 50 73 98'
+	'python ../../train.py --optimizer=ENGD --ENGD_approximation=full --ENGD_damping=1e-10 --ENGD_ema_factor=0.3 --ENGD_initialize_to_identity --num_seconds=100 --plot_solution --plot_dir=ENGD_full --plot_steps 3 6 9 11'
+	'python ../../train.py --optimizer=ENGD --ENGD_approximation=per_layer --ENGD_damping=0 --ENGD_ema_factor=0.9 --ENGD_initialize_to_identity --num_seconds=100 --plot_solution --plot_dir=ENGD_layer-wise --plot_steps 3 6 9 12'
+	'python ../../train.py --optimizer=ENGD --ENGD_approximation=diagonal --ENGD_damping=0.0001 --ENGD_ema_factor=0.3 --num_seconds=100 --plot_solution --plot_dir=ENGD_diagonal --plot_steps 3 7 10 13'
+	'python ../../train.py --optimizer=KFAC --KFAC_damping=1.5440986717145857e-12 --KFAC_ema_factor=0.4496490467123582 --KFAC_initialize_to_identity --KFAC_momentum=0.5117574706015794 --num_seconds=100 --plot_solution --plot_dir=KFAC --plot_steps 10 20 31 40'
+	'python ../../train.py --optimizer=KFAC --KFAC_damping=1.2156399458704562e-10 --KFAC_ema_factor=0.9263313896660544 --KFAC_initialize_to_identity --KFAC_lr=auto --num_seconds=100 --plot_solution --plot_dir=KFAC_auto --plot_steps 17 35 50 67'
 )
 
 CMD=${JOBS[$SLURM_ARRAY_TASK_ID]}
