4bd917b1387e415d61c789d183bf4a37746d9093
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-08-09 10:57:52 +0200

    [ADD] Runs for exp27

--- kfac_pinns_exp/exp42_visualize_solutions/create_launch_script.py
+++ kfac_pinns_exp/exp42_visualize_solutions/create_launch_script.py
@@ -9,13 +9,14 @@ from typing import Dict
 
 from numpy import absolute
 
-from kfac_pinns_exp.exp09_reproduce_poisson2d.plot import (
-    DATADIR,
-    entity,
-    project,
-    sweep_ids,
-)
+from kfac_pinns_exp.exp09_reproduce_poisson2d.plot import DATADIR as exp09_DATADIR
+from kfac_pinns_exp.exp09_reproduce_poisson2d.plot import entity
+from kfac_pinns_exp.exp09_reproduce_poisson2d.plot import project as exp09_project
+from kfac_pinns_exp.exp09_reproduce_poisson2d.plot import sweep_ids as exp09_sweep_ids
 from kfac_pinns_exp.exp09_reproduce_poisson2d.yaml_to_sh import QUEUE_TO_TIME
+from kfac_pinns_exp.exp27_heat4d_small.plot import DATADIR as exp27_DATADIR
+from kfac_pinns_exp.exp27_heat4d_small.plot import project as exp27_project
+from kfac_pinns_exp.exp27_heat4d_small.plot import sweep_ids as exp27_sweep_ids
 from kfac_pinns_exp.wandb_utils import load_best_run
 
 HEREDIR = path.dirname(path.abspath(__file__))
@@ -35,65 +36,72 @@ def get_commands(local_files: bool = False) -> Dict[str, str]:
     """
     commands = {}
 
-    for sweep_id, optim in sweep_ids.items():
-        # load meta-data of the run
-        df_history, df_meta = load_best_run(
-            entity,
-            project,
-            sweep_id,
-            save=False,
-            update=not local_files,
-            savedir=DATADIR,
-        )
-        dict_config = df_meta.to_dict()["config"][0]
-        # Loading from saved file returns a string representation that needs to
-        # be evaluated into a dict.
-        if local_files:
-            dict_config = literal_eval(dict_config)
-        run_cmd = dict_config["cmd"].split(" ")
+    experiments = [
+        (exp09_DATADIR, exp09_project, exp09_sweep_ids),
+        (exp27_DATADIR, exp27_project, exp27_sweep_ids),
+    ]
 
-        # find out time and decrease it
-        (time_arg,) = [arg for arg in run_cmd if "--num_seconds" in arg]
-        time_arg = int(time_arg.split("=")[1])
-        shorter_time = int(0.2 * time_arg)
+    for DATADIR, project, sweep_ids in experiments:
 
-        # drop time and wandb arguments
-        run_cmd = [
-            arg
-            for arg in run_cmd
-            if "--wandb" not in arg and "--num_seconds" not in arg
-        ]
+        for sweep_id, optim in sweep_ids.items():
+            # load meta-data of the run
+            df_history, df_meta = load_best_run(
+                entity,
+                project,
+                sweep_id,
+                save=False,
+                update=not local_files,
+                savedir=DATADIR,
+            )
+            dict_config = df_meta.to_dict()["config"][0]
+            # Loading from saved file returns a string representation that needs to
+            # be evaluated into a dict.
+            if local_files:
+                dict_config = literal_eval(dict_config)
+            run_cmd = dict_config["cmd"].split(" ")
 
-        # we want to visualize at initialization, around 10% of training, 50% of
-        # training, and at the end of training
-        visualize_ratios = [0, 0.001, 0.01, 0.1]
-        visualize_steps = []
-        # find the closest point
-        logged_steps = df_history["step"].to_numpy()
-        max_step = max(logged_steps)
+            # find out time and decrease it
+            (time_arg,) = [arg for arg in run_cmd if "--num_seconds" in arg]
+            time_arg = int(time_arg.split("=")[1])
+            shorter_time = int(0.2 * time_arg)
 
-        # select closest point
-        for ratio in visualize_ratios:
-            idx = absolute(logged_steps - ratio * max_step).argmin()
-            visualize_steps.append(str(logged_steps[idx]))
-
-        # plotting directory
-        plot_dir = (
-            optim.replace("(", "")
-            .replace(")", "")
-            .replace(" ", "_")
-            .replace("*", "_auto")
-        )
-
-        commands[sweep_id] = " ".join(
-            run_cmd
-            + [
-                f"--num_seconds={shorter_time}",
-                "--plot_solution",
-                f"--plot_dir={plot_dir}",
-                f"--plot_steps {' '.join(visualize_steps)}",
+            # drop time and wandb arguments
+            run_cmd = [
+                arg
+                for arg in run_cmd
+                if "--wandb" not in arg and "--num_seconds" not in arg
             ]
-        )
+
+            # we want to visualize at initialization, around 10% of training, 50% of
+            # training, and at the end of training
+            visualize_ratios = [0, 0.001, 0.01, 0.1]
+            visualize_steps = []
+            # find the closest point
+            logged_steps = df_history["step"].to_numpy()
+            max_step = max(logged_steps)
+
+            # select closest point
+            for ratio in visualize_ratios:
+                idx = absolute(logged_steps - ratio * max_step).argmin()
+                visualize_steps.append(str(logged_steps[idx]))
+
+            # plotting directory
+            plot_dir = (
+                optim.replace("(", "")
+                .replace(")", "")
+                .replace(" ", "_")
+                .replace("*", "_auto")
+            )
+
+            commands[sweep_id] = " ".join(
+                run_cmd
+                + [
+                    f"--num_seconds={shorter_time}",
+                    "--plot_solution",
+                    f"--plot_dir={plot_dir}",
+                    f"--plot_steps {' '.join(visualize_steps)}",
+                ]
+            )
 
     return commands
 
