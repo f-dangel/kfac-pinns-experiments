39ad352ff898aa1f2a2a1bd1bba5ff6554284d8c
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-08-14 15:20:31 -0400

    [REF] Update KFAC sweeps [no ci]

--- kfac_pinns_exp/exp43_log_fokker_planck9d_isotropic_gaussian_random/plot.py
+++ kfac_pinns_exp/exp43_log_fokker_planck9d_isotropic_gaussian_random/plot.py
@@ -41,8 +41,8 @@ sweep_ids = {  # ids from the wandb agent
     "m7m8c4ng": "Adam",
     "g0tshg12": "Hessian-free",
     "7ktbx4hp": "LBFGS",
-    "k09ikdxb": "KFAC",
-    "yi2m09hd": "KFAC*",
+    "k064uq94": "KFAC",
+    "z77lkpas": "KFAC*",
 }
 
 # color options: https://jiffyclub.github.io/palettable/colorbrewer/
