b6873f8c4a4d80e4dae69094c66c8cf34c54571f
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-08-14 11:13:43 -0400

    [REF] Update KFAC sweeps for exp43 [no ci]

--- kfac_pinns_exp/exp43_log_fokker_planck9d_isotropic_gaussian_random/sweeps/KFAC_auto.sh
+++ kfac_pinns_exp/exp43_log_fokker_planck9d_isotropic_gaussian_random/sweeps/KFAC_auto.sh
@@ -13,4 +13,4 @@ echo "[DEBUG] Host name: " `hostname`
 source  ~/anaconda3/etc/profile.d/conda.sh
 conda activate kfac_pinns_exp
 
-wandb agent --count 1 kfac-pinns/log_fokker_planck9d_isotropic_gaussian_random/d7sjofx0
+wandb agent --count 1 kfac-pinns/log_fokker_planck9d_isotropic_gaussian_random/6qie947v
