00752571947941788e05b113af18391a3d2cfa89
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-09-24 14:25:40 -0400

    [ADD] Compare leading eigvals of G and JJT for multiple settings

--- test/optim/test_spring.py
+++ test/optim/test_spring.py
@@ -1,6 +1,10 @@
 """Test SPRING-related functionality."""
 
-from torch import allclose, cuda, device, eye, float64, isclose, manual_seed
+from test.utils import DEVICE_IDS, DEVICES, report_nonclose
+from typing import List, Tuple
+
+from pytest import mark
+from torch import device, dtype, eye, float64, manual_seed
 from torch.linalg import eigvalsh
 
 from kfac_pinns_exp.linops import GramianLinearOperator
@@ -14,67 +18,86 @@ from kfac_pinns_exp.train import (
     set_up_layers,
 )
 
-# make sure the eigenvalues of the Gramian and Jacobian outer product match
+PDE_CASES: List[Tuple[str, str]] = [
+    ("poisson", "sin_product"),
+    ("poisson", "cos_sum"),
+    ("heat", "sin_product"),
+    ("fokker-planck-isotropic", "gaussian"),
+    ("log-fokker-planck-isotropic", "gaussian"),
+]
+PDE_IDS = [f"{equation}-{condition}" for equation, condition in PDE_CASES]
+MODEL_CASES = ["mlp-tanh-64"]
 
-# hyper-parameters
-equation = "poisson"
-boundary_condition = "sin_product"
-N_Omega = 64
-N_dOmega = 32
-dim_Omega = 2
-model = "mlp-tanh-64"
-dt = float64
-dev = device("cuda" if cuda.is_available() else "cpu")
 
-manual_seed(0)  # make deterministic
+@mark.parametrize("device", DEVICES, ids=DEVICE_IDS)
+@mark.parametrize("model", MODEL_CASES, ids=MODEL_CASES)
+@mark.parametrize("equation, condition", PDE_CASES, ids=PDE_IDS)
+def test_leading_eigenvalues_G_and_JJT(
+    equation: str,
+    condition: str,
+    model: str,
+    device: device,
+    dtype: dtype = float64,
+    N_Omega: int = 64,
+    N_dOmega: int = 32,
+    dim_Omega: int = 3,
+):
+    """Make sure the leading Gramian and Jacobian outer product eigenvalues match.
 
-# generate neural network and data
-layers = set_up_layers(model, equation, dim_Omega)
-layers = [layer.to(dev, dt) for layer in layers]
+    Args:
+        equation: String specifying the PDE.
+        condition: String specifying the conditions.
+        model: String specifying the model.
+        device: The device to run the test on.
+        dtype: The data type to run the test in. Defaults to `float64`.
+        N_Omega: The number of interior data points. Defaults to `64`.
+        N_dOmega: The number of boundary data points. Defaults to `32`.
+        dim_Omega: The dimension of the domain. Defaults to `3`.
+    """
+    manual_seed(0)  # make deterministic
 
-X_Omega, y_Omega = create_interior_data(
-    equation, boundary_condition, dim_Omega, N_Omega
-)
-X_Omega, y_Omega = X_Omega.to(dev, dt), y_Omega.to(dev, dt)
-X_dOmega, y_dOmega = create_condition_data(
-    equation, boundary_condition, dim_Omega, N_dOmega
-)
-X_dOmega, y_dOmega = X_dOmega.to(dev, dt), y_dOmega.to(dev, dt)
+    # generate neural network and data
+    layers = [
+        layer.to(device, dtype) for layer in set_up_layers(model, equation, dim_Omega)
+    ]
+    num_params = sum(sum(p.numel() for p in layer.parameters()) for layer in layers)
 
-num_params = sum(sum(p.numel() for p in layer.parameters()) for layer in layers)
+    X_Omega, y_Omega = [
+        t.to(device, dtype)
+        for t in create_interior_data(equation, condition, dim_Omega, N_Omega)
+    ]
+    X_dOmega, y_dOmega = [
+        t.to(device, dtype)
+        for t in create_condition_data(equation, condition, dim_Omega, N_dOmega)
+    ]
 
-# ground truth: Eigenvalues of the Gramian
-G_interior = GramianLinearOperator(equation, layers, X_Omega, y_Omega, "interior")
-G_boundary = GramianLinearOperator(equation, layers, X_dOmega, y_dOmega, "boundary")
+    # ground truth: Eigenvalues of the Gramian
+    G_interior = GramianLinearOperator(equation, layers, X_Omega, y_Omega, "interior")
+    G_boundary = GramianLinearOperator(equation, layers, X_dOmega, y_dOmega, "boundary")
 
-identity = eye(num_params, device=dev, dtype=dt)
-G = G_interior @ identity + G_boundary @ identity
-G_evals = eigvalsh(G)
+    identity = eye(num_params, device=device, dtype=dtype)
+    G = G_interior @ identity + G_boundary @ identity
+    G_evals = eigvalsh(G)
 
-# compare with: Eigenvalues of the Jacobian outer product
-_, _, interior_inputs, interior_grad_outputs, boundary_inputs, boundary_grad_outputs = (
-    evaluate_losses_with_layer_inputs_and_grad_outputs(
+    # compare with: Eigenvalues of the Jacobian outer product
+    (
+        _,
+        _,
+        interior_inputs,
+        interior_grad_outputs,
+        boundary_inputs,
+        boundary_grad_outputs,
+    ) = evaluate_losses_with_layer_inputs_and_grad_outputs(
         layers, X_Omega, y_Omega, X_dOmega, y_dOmega, equation
     )
-)
-JJT = compute_jacobian_outer_product(
-    interior_inputs, interior_grad_outputs, boundary_inputs, boundary_grad_outputs
-)
-JJT_evals = eigvalsh(JJT)
-
-# clip to same length and sort descendingly
-effective_evals = min(len(G_evals), len(JJT_evals))
-
-G_evals = G_evals.flip(0)[:effective_evals]
-JJT_evals = JJT_evals.flip(0)[:effective_evals]
-
-# comparison
-if not allclose(G_evals, JJT_evals):
-    for idx, (e_G, e_JJT) in enumerate(zip(G_evals, JJT_evals)):
-        print(
-            f"{idx:03g}: {e_G.item():.5g} vs {e_JJT.item():.5g} "
-            + f"-> {isclose(e_G, e_JJT).item()}"
-        )
-    raise ValueError(
-        "Eigenvalues of the Gramian and Jacobian outer product do not match."
+    JJT = compute_jacobian_outer_product(
+        interior_inputs, interior_grad_outputs, boundary_inputs, boundary_grad_outputs
     )
+    JJT_evals = eigvalsh(JJT)
+
+    # clip to same length and sort descendingly
+    effective_evals = min(num_params, N_Omega + N_dOmega)
+    G_evals = G_evals.flip(0)[:effective_evals]
+    JJT_evals = JJT_evals.flip(0)[:effective_evals]
+
+    report_nonclose(G_evals, JJT_evals)
