5b5281b01930b2a481bda1ae614f215e2279059e
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-10-16 17:13:37 -0400

    [ADD] Fine-tune search space of Hessian-free on exp32 [no ci]

--- kfac_pinns_exp/exp32_poisson10d_mlp_tanh_256_bayes/sweeps/HessianFree.sh
+++ kfac_pinns_exp/exp32_poisson10d_mlp_tanh_256_bayes/sweeps/HessianFree.sh
@@ -13,4 +13,4 @@ echo "[DEBUG] Host name: " `hostname`
 source  ~/anaconda3/etc/profile.d/conda.sh
 conda activate kfac_pinns_exp
 
-wandb agent --count 1 kfac-pinns/poisson10d_mlp_tanh_256_bayes/68ttowe8
+wandb agent --count 1 kfac-pinns/poisson10d_mlp_tanh_256_bayes/cdjadvh6
