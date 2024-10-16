c46f4bae69266d1b180dd740dbff5ab62cccdefa
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-10-16 14:45:35 -0400

    [ADD] Fine-tune Hessianfree hyper-params for exp09 [no ci]

--- kfac_pinns_exp/exp09_reproduce_poisson2d/sweeps/HessianFree.sh
+++ kfac_pinns_exp/exp09_reproduce_poisson2d/sweeps/HessianFree.sh
@@ -13,4 +13,4 @@ echo "[DEBUG] Host name: " `hostname`
 source  ~/anaconda3/etc/profile.d/conda.sh
 conda activate kfac_pinns_exp
 
-wandb agent --count 1 kfac-pinns/poisson2d/268clvk7
+wandb agent --count 1 kfac-pinns/poisson2d/0uu552mg
