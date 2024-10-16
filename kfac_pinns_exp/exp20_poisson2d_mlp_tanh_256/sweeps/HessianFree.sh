ba15d166134e644a2b16a7c6b02a83d8cddcd74a
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-10-16 15:22:41 -0400

    [ADD] Fine-tune Hessian-free hyper-parameters for exp20 [no ci]

--- kfac_pinns_exp/exp20_poisson2d_mlp_tanh_256/sweeps/HessianFree.sh
+++ kfac_pinns_exp/exp20_poisson2d_mlp_tanh_256/sweeps/HessianFree.sh
@@ -13,4 +13,4 @@ echo "[DEBUG] Host name: " `hostname`
 source  ~/anaconda3/etc/profile.d/conda.sh
 conda activate kfac_pinns_exp
 
-wandb agent --count 1 kfac-pinns/poisson2d_mlp_tanh_256/s9pj2ugv
+wandb agent --count 1 kfac-pinns/poisson2d_mlp_tanh_256/cyswa16b
