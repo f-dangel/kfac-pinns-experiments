ffbaa0a6c29f3ef7d76be5528510197b4b9f8f00
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-10-28 11:25:07 -0400

    [REF] Bump run id [no ci]

--- kfac_pinns_exp/exp15_poisson2d_deepwide/runs/HessianFreeCached.sh
+++ kfac_pinns_exp/exp15_poisson2d_deepwide/runs/HessianFreeCached.sh
@@ -10,4 +10,4 @@
 
 echo "[DEBUG] Host name: " `hostname`
 
-python ../../train.py --optimizer=HessianFreeCached --HessianFreeCached_cg_max_iter=350 --HessianFreeCached_damping=0.1 --boundary_condition=cos_sum --model=mlp-tanh-64-64-48-48 --num_seconds=1000 --wandb --wandb_entity=kfac-pinns --wandb_project=poisson2d_deepwide --wandb_id=HessianFreeCached-01
+python ../../train.py --optimizer=HessianFreeCached --HessianFreeCached_cg_max_iter=350 --HessianFreeCached_damping=0.1 --boundary_condition=cos_sum --model=mlp-tanh-64-64-48-48 --num_seconds=1000 --wandb --wandb_entity=kfac-pinns --wandb_project=poisson2d_deepwide --wandb_id=HessianFreeCached-02
